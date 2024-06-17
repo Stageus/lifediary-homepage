69c2cfb94f448b0f72eae8e1e2281e56a09e0ce1
junik <wkdwnsdlr37@gmail.com>
2024-06-17 21:20:23 +0900

    Docs: shared layer 기본구조 설정

---
+++ src/5_shared/ui/defaultInput/DefaultInput.jsx
@@ -0,0 +1,7 @@
+import "./style";
+export const DefaultInput = ()=>{
+    return(
+        <>
+        </>
+    );
+}
---
+++ src/5_shared/ui/index.js
@@ -0,0 +1,6 @@
+export { Icon } from "./Icon";
+export { defaultInput } from "./defaultInput";
+export { diaryContent } from "./diaryContent";
+export { diaryThumbnail } from "./diaryThumbnail";
+export { tagInput } from "./tagInput";
+export { textInput } from "./textInput";
