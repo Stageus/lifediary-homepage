80cca1a1d7ae84e86ba70d69fcfadb89e6bde125
junik <wkdwnsdlr37@gmail.com>
2024-06-18 18:10:34 +0900

    Docs: shared index.js export 설정

--- src/5_shared/ui/index.js
+++ src/5_shared/ui/index.js
@@ -4,3 +4,4 @@ export { DiaryContent } from "./diaryContent/DiaryContent";
 export { DiaryThumbnail } from "./diaryThumbnail/DiaryThumbnail";
 export { TagInput } from "./tagInput/TagInput";
 export { TextInput } from "./textInput/TextInput";
+export { DefaultBtn } from "./defaultBtn/DefaultBtn";
