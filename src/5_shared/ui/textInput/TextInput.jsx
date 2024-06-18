15850736cc9dc05bebc754fa6c80e2d4d32b6968
mangkong1 <xogud5053@hanmail.net>
2024-06-18 19:41:37 +0900

    Refactor: style코드 export부분 수정, style-components컨벤션 형태 적용

--- src/5_shared/ui/textInput/TextInput.jsx
+++ src/5_shared/ui/textInput/TextInput.jsx
@@ -1,9 +1,10 @@
-import { STextInput } from "./style";
+import S from "./style";
 
 export const TextInput = (props) => {
   return (
     <>
-      <STextInput type="text" placeholder={props.placeholder} $variant={props.variant} />
+      {/* variant prop이 DOM요소로 전달되어, 콘솔 오류를 유발할 수 있어 앞에$를 붙임, 이렇게 하면 prop을 스타일링에만 사용가능 */}
+      <S.textInput type="text" placeholder={props.placeholder} $variant={props.variant} />
     </>
   );
 };
--- src/5_shared/ui/textInput/style.js
+++ src/5_shared/ui/textInput/style.js
@@ -1,22 +1,24 @@
 import styled from "styled-components";
 
-export const STextInput = styled.input`
-  ${({ $variant, theme }) =>
-    $variant === "select" &&
-    `
+export default {
+  textInput: styled.input`
+    ${({ $variant, theme }) =>
+      $variant === "select" &&
+      `
     background-color: ${theme.major};
     border: 1px solid ${theme.white};
   `}
-  ${({ $variant, theme }) =>
-    $variant === "disabled" &&
-    `
+    ${({ $variant, theme }) =>
+      $variant === "disabled" &&
+      `
     background-color: ${theme.gray};
     border: 1px solid ${theme.white};
   `}
   ${({ $variant, theme }) =>
-    $variant === "default" &&
-    `
+      $variant === "default" &&
+      `
     background-color: ${theme.highlight};
     border: 1px solid ${theme.white};
   `}
-`;
+  `,
+};
