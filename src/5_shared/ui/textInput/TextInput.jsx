1e6f7f4551d6cb1cc584aaeb8b6f462eeacda2e0
mangkong1 <xogud5053@hanmail.net>
2024-06-18 23:44:53 +0900

    Refactor: css 조정을 위한 props생성, focus아웃시 Input값을 받아오는 이벤트함수 생성

--- src/5_shared/ui/textInput/TextInput.jsx
+++ src/5_shared/ui/textInput/TextInput.jsx
@@ -1,10 +1,17 @@
+import { useRef } from "react";
 import { S } from "./style";
 
 export const TextInput = (props) => {
+  const { width, height, px, py, fontSize, placeholder, borderWidth, variant } = props;
+  const textInputRef = useRef(); // 입력 필드에 대한 참조 생성
+
+  const handleBlur = () => {
+    console.log(textInputRef.current.value);
+  };
+
   return (
     <>
-      {/* variant prop이 DOM요소로 전달되어, 콘솔 오류를 유발할 수 있어 앞에$를 붙임, 이렇게 하면 prop을 스타일링에만 사용가능 */}
-      <S.TextInput type="text" placeholder={props.placeholder} $variant={props.variant} />
+      <S.TextInput type="text" width={width} height={height} px={px} py={py} fontSize={fontSize} placeholder={placeholder} borderWidth={borderWidth} $variant={variant} ref={textInputRef} onBlur={handleBlur} />
     </>
   );
 };
--- src/5_shared/ui/textInput/style.js
+++ src/5_shared/ui/textInput/style.js
@@ -1,22 +1,18 @@
 import styled from "styled-components";
 
 const TextInput = styled.input`
-  ${({ theme }) => `
+  ${({ theme, width, px, py, fontSize, borderWidth }) => `
+    width: ${width || "100%"};
     height: 40px;
-    padding: 8px 16px;
     border-radius: 10px;
-    font-size: ${theme.fontSize.base};
+    padding: ${px || "8px"} ${py || "16px"};
+    font-size: ${fontSize || theme.fontSize.base};
     color: ${theme.black};
+    background-color: ${theme.white};
+    border: ${borderWidth || "3px"} solid ${theme.major};
     `}
 
   ${({ $variant, theme }) =>
-    $variant === "default" &&
-    `
-      background-color: ${theme.white};
-      border: 3px solid ${theme.major};
-    `}
-
-    ${({ $variant, theme }) =>
     $variant === "error" &&
     `
       background-color: ${theme.white};
