98933b46cd12fa17082c28830184a590c131bea3
junik <wkdwnsdlr37@gmail.com>
2024-06-19 16:36:23 +0900

    Design: Slider 디자인 적용

---
+++ src/4_features/slider/ui/style.js
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+const Slider = styled.div`
+    width: 90%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+`;
+
+const Button = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    background-color: ${({theme})=> theme.major};
+    cursor: pointer;
+`;
+
+const ItemList = styled.div`
+    display: flex;
+    flex: 1;
+    overflow: hidden;
+`;
+
+const ItemContainer = styled.div`
+    flex-shrink: 0;
+    width: 100%;
+    display: flex;
+    gap: 10px;
+    padding: 10px;
+`;
+
+const Item = styled.div`
+    flex: 1;
+    box-shadow: 3px 3px 7px gray;
+    border-radius: 10px;
+    & > img {
+        border-radius: 10px;
+        width: 100%;
+    }
+`;
+
+const UserInfo = styled.div`
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        padding: 10px;
+        gap: 10px;
+        & > img{
+            width: 25px;
+        }
+        & > span{
+            color: ${({theme}) => theme.black};
+            font-size: ${({theme}) => theme.fontSize.medium};
+        }
+`
+
+
+
+export const S = {
+    Slider,
+    Button,
+    ItemList,
+    ItemContainer,
+    Item,
+    UserInfo,
+}
