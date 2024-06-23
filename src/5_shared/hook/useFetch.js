7e789ed868a58965bdbb5f27f683d6f3aadec47c
mangkong1 <xogud5053@hanmail.net>
2024-06-23 22:15:20 +0900

    Feat: useFetch 생성

--- src/5_shared/hook/useFetch.js
+++ src/5_shared/hook/useFetch.js
@@ -1,3 +1,34 @@
-export const useFetch = ()=>{
-    
-}
+import { useState, useEffect } from "react";
+
+export const useFetch = (url, method, autoFetch = false, body) => {
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url, {
+        method: method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setData(result);
+      } else {
+        console.error("Error:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+  }, []);
+
+  return { data, fetchData };
+};
