49012d86590afb0835b08d09b40b20e912049a75
buzzkim <97722152+mangkong1@users.noreply.github.com>
2024-06-24 12:47:09 +0900

    useCookie 생성 (#9)
    * Feat: useCookie 생성
    
    * Refactor: getCookie이벤트 코드 수정

--- src/5_shared/hook/useCookie.js
+++ src/5_shared/hook/useCookie.js
@@ -1,2 +1,21 @@
-export const useCookie = ()=>{
-}
+import { useCookies } from "react-cookie";
+
+export const useCookie = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(["myCookie"]);
+
+  const handleSetCookie = (name, value) => {
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 1);
+    setCookie(name, value, { expires });
+  };
+
+  const handleGetCookie = () => {
+    return cookies.myCookie;
+  };
+
+  const handleRemoveCookie = (name) => {
+    removeCookie(name);
+  };
+
+  return { handleSetCookie, handleGetCookie, handleRemoveCookie };
+};
