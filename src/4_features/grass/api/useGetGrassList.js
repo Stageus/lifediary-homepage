0f9619be7762e12edd754aadab4b836c50d0223e
junik <wkdwnsdlr37@gmail.com>
2024-06-26 20:21:58 +0900

    feat: useGetGrassList 구조작성

--- src/4_features/grass/api/useGetGrassList.js
+++ src/4_features/grass/api/useGetGrassList.js
@@ -1,3 +1,25 @@
+import { useEffect, useState } from "react";
+import {useFetch, useCookie} from "@shared/hook";
+
 export const useGetGrassList = ()=>{
-    return("");
+    const [data, error, baseFetch] = useFetch();
+    const [selectYear, setSelectYear] = useState(null);
+    const {handleGetCookie} = useCookie();
+
+
+    const getGrassList = ()=>{
+        if(!selectYear){
+            baseFetch("grass",{},handleGetCookie());
+            return;
+        }
+        baseFetch(`grass?year=${selectYear}`,{},handleGetCookie());
+    }
+
+    useEffect(()=>{
+        getGrassList();
+        if(error){
+            return "에러 바운더리 대기"
+        }
+    },[selectYear])
+    return[data, setSelectYear];
 }
