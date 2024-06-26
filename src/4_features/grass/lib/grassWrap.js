9976540f28feacf6a05a2763bce22b6dd0e064b6
junik <wkdwnsdlr37@gmail.com>
2024-06-26 23:42:13 +0900

    Refactor: grassWrap 함수 조건 수정

--- src/4_features/grass/lib/grassWrap.js
+++ src/4_features/grass/lib/grassWrap.js
@@ -9,18 +9,15 @@ export const grassWrap = (grassList)=>{
         "금":[],
         "토":[],
     };
-    
     for(let grass of grassList){
         const grassDay = new Date(grass.date).getDay();
         const dayName = daysOfWeek[grassDay];
         result[dayName].push(grass);
     }
-    
-    for(let i = 0 ; i < daysOfWeek.length - 1 ; i++){
-        if(result[daysOfWeek[i]].length >= result[daysOfWeek[i + 1]].length) break;
-        result[daysOfWeek[i]].unshift(null);
-    }
-    
+
     const resultWrap = Object.keys(result).map( day => ({[day]:result[day]}));
+    const dayIndex = resultWrap.findIndex((day) => day[Object.keys(day)[0]].length === 53);
+    for(let i = 0 ; i < dayIndex ; i++) result[daysOfWeek[i]].unshift(null);
+    
     return resultWrap;
 };
