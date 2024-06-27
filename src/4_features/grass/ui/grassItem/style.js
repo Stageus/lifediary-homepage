7ac338c24c6d20b330c9ff6ad0a511a4d3dbb5c1
junik <wkdwnsdlr37@gmail.com>
2024-06-27 15:25:22 +0900

    Docs: GrassItem 파일분리

---
+++ src/4_features/grass/ui/grassItem/style.js
@@ -0,0 +1,40 @@
+import styled,{css} from "styled-components";
+
+const GrassItem = styled.div`
+    width: 15px;
+    height: 15px;
+    border-radius: 2px;
+    cursor: pointer;
+    ${({ $date, $isExist, theme }) => $date 
+    ? css`
+        background-color: ${$isExist ? $isExist : theme.gray};
+      `
+    : css`
+        background-color: transparent;
+      `
+    }
+
+`;
+
+const MonthItem = styled.div`
+    white-space: nowrap;
+    position: absolute;
+    top: 0;
+    
+`;
+
+const GrassItemInfo = styled.div`
+    position: fixed;
+    border-radius: 5px;
+    transform: translate(0, -110%);
+    background-color: ${({theme}) => theme.gray};
+    color: ${({theme})=> theme.white};
+    padding: 5px;
+    width: fit-content;
+`;
+
+export const S = {
+    GrassItem,
+    MonthItem,
+    GrassItemInfo
+}
