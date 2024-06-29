1bfae416866abca56861b4e9e5567d39f5463d1e
junik <wkdwnsdlr37@gmail.com>
2024-06-29 18:16:28 +0900

    Design: UI 구조설정

--- src/3_widgets/aside/ui/Aside.jsx
+++ src/3_widgets/aside/ui/Aside.jsx
@@ -1,8 +1,22 @@
-import "./style";
-export const Aside = ()=>{
-    return(
-        <>
-            <aside>aside</aside>
-        </>
-    );
-}
+import { S } from "./style";
+import { DefaultBtn } from "@shared/ui";
+export const Aside = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <DefaultBtn text="홈" />
+          <DefaultBtn text="둘러보기" />
+          <DefaultBtn text="신고보기" />
+        </div>
+        <div>
+          <h3>구독목록</h3>
+          <div>
+            <img src="#" alt="#" />
+            <span>피카츄</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
