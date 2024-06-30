5f67909fe2556dff835905138ba6cb9780ea106b
junik <wkdwnsdlr37@gmail.com>
2024-06-30 14:46:29 +0900

    Design: UI 생성

--- src/4_features/subscribeList/ui/SubscribeList.jsx
+++ src/4_features/subscribeList/ui/SubscribeList.jsx
@@ -1,9 +1,25 @@
 import { S } from "./style";
+import { createTestData } from "../model/createTestData";
 
 export const SubscribeList = ()=>{
+    const testData = createTestData();
     return(
         <>
-            <div>구독리스트</div>
+            <S.SubscribeInfo>
+          <S.SubscribeTitle>구독목록</S.SubscribeTitle>
+
+          <S.SubscribeList>
+          {testData && testData.map((value)=>{
+            return(
+                <S.SubscribeItem key={value.idx}>
+                    <img src={value.profileImg} alt="#" />
+                    <span>{value.nickname}</span>
+                </S.SubscribeItem>
+            );
+          })}
+          </S.SubscribeList>
+          
+        </S.SubscribeInfo>
         </>
     );
 }
