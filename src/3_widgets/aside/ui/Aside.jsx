d26456f8cdc4909ca6b215a6333c318a93fd6006
Jangmy <108041161+JangIkIk@users.noreply.github.com>
2024-06-30 13:58:01 +0900

    Complain alarm (#18)
    * Design: UI 구조설정
    
    * Docs: createTestData 함수 생성
    
    * Design: TestDat적용및 css변경
    
    * Design: Css 변경
    
    * Design: Layout 임시 라인 삭제
    
    * None
    
    * Refactor: 주석 삭제
    
    * Docs: ComplainAlarm 기본구조생성
    
    * Design: UI 생성
    
    * None
    
    * Feat: useGetAlarm fetch 적용

--- src/3_widgets/aside/ui/Aside.jsx
+++ src/3_widgets/aside/ui/Aside.jsx
@@ -1,8 +1,37 @@
-import "./style";
-export const Aside = ()=>{
-    return(
-        <>
-            <aside>aside</aside>
-        </>
-    );
-}
+import { useLocation } from "react-router-dom";
+import { S } from "./style";
+import { createTestData } from "../model/createTestData";
+import { DefaultBtn } from "@shared/ui";
+
+export const Aside = () => {
+    const testData = createTestData();
+    const test = useLocation();
+
+  return (
+    <>
+      <S.Aside>
+        <S.BtnList>
+          <DefaultBtn text="홈" />
+          <DefaultBtn text="둘러보기" />
+          <DefaultBtn text="신고보기" />
+        </S.BtnList>
+
+        <S.SubscribeInfo>
+          <S.SubscribeTitle>구독목록</S.SubscribeTitle>
+
+          <S.SubscribeList>
+          {testData && testData.map((value)=>{
+            return(
+                <S.SubscribeItem key={value.idx}>
+                    <img src={value.profileImg} alt="#" />
+                    <span>{value.nickname}</span>
+                </S.SubscribeItem>
+            );
+          })}
+          </S.SubscribeList>
+          
+        </S.SubscribeInfo>
+      </S.Aside>
+    </>
+  );
+};
