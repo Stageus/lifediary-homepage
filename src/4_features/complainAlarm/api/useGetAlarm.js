00d39521928778b6bf27c4f2caa71823d1a79adc
junik <wkdwnsdlr37@gmail.com>
2024-06-30 13:57:09 +0900

    Feat: useGetAlarm fetch 적용

--- src/4_features/complainAlarm/api/useGetAlarm.js
+++ src/4_features/complainAlarm/api/useGetAlarm.js
@@ -1,6 +1,33 @@
+import { useEffect, useState } from "react";
+
+
+import { useFetch, useCookie } from "@shared/hook";
 
 export const useGetAlarm = () => {
+    // 임시데이터
+    const [testData, setTestData] = useState({
+		"isNew": true
+	});
+    const [fetchData, errorStatus, baseFetch] = useFetch();
+    const { handleGetCookie } = useCookie();
 
+    const getAlarm = ()=>{
+        baseFetch("report/new",{},handleGetCookie());
+    }
 
-    return [];
+    useEffect(()=>{
+        // 임시주석
+        // getAlarm();
+
+        if(errorStatus){
+            return "에러바운더리 대기";
+        }
+        
+        // 알림은 페이지가 새로고침시에만 발생시키기 때문에
+        // 의존성은 할당하지 않는다
+    },[])
+
+    // 임시데이터
+    return testData.isNew;
+    // return fetchData.isNew;
 };
--- src/4_features/complainAlarm/ui/ComplainAlarm.jsx
+++ src/4_features/complainAlarm/ui/ComplainAlarm.jsx
@@ -1,19 +1,28 @@
+import { useNavigate } from "react-router-dom";
 import { S } from "./style";
+import { useGetAlarm } from "../api/useGetAlarm";
 import { DefaultBtn, Icon } from "@shared/ui";
 
 export const ComplainAlarm = () => {
+    const isAlarm = useGetAlarm();
+    const navigate = useNavigate();
+    const onClickRoute = () => navigate("complain");
+
     return(
         <>
             <S.ComplainAlarm>
                 <DefaultBtn
                 text="신고 보기"
+                onClick={onClickRoute}
                 />
-                <S.Alarm>
+                {isAlarm 
+                ? <S.Alarm>
                     <Icon
                     type="alarm"
                     color="white"
                     />
-                </S.Alarm>
+                </S.Alarm> 
+                : null}
             </S.ComplainAlarm>
         </>
     );
