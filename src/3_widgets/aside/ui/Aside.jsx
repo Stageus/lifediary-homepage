00209cb83c2639a31bf7c99ffddc9b5baab002a1
junik <wkdwnsdlr37@gmail.com>
2024-07-01 11:20:47 +0900

    Feat: Route 이벤트 설정

--- src/3_widgets/aside/ui/Aside.jsx
+++ src/3_widgets/aside/ui/Aside.jsx
@@ -1,15 +1,27 @@
+import { useNavigate, useLocation } from "react-router-dom";
 import { S } from "./style";
 import { DefaultBtn } from "@shared/ui";
 import { ComplainAlarm  } from "@features/complainAlarm"; 
 import { SubscribeList } from "@features/subscribeList";
 
 export const Aside = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const onClickRoute = (path) => navigate(path)
+  
   return (
     <>
       <S.Aside>
         <S.BtnList>
-          <DefaultBtn text="홈" />
-          <DefaultBtn text="둘러 보기" />
+          <DefaultBtn 
+          text="홈"
+          onClick={()=>onClickRoute("/")}
+           />
+          <DefaultBtn 
+            text="둘러 보기"
+            onClick={()=>onClickRoute("diary")}
+            type={location.pathname === "/diary" ? "select" : null}
+             />
           <ComplainAlarm/>
         </S.BtnList>
         <SubscribeList/>
