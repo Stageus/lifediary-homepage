bb01ee2cdba44f1836de617f8733322e3927110f
Jangmy <108041161+JangIkIk@users.noreply.github.com>
2024-07-01 11:26:23 +0900

    Complain alarm Route 색상적용 (#22)
    * Design: UI 구조설정
    
    * Docs: createTestData 함수 생성
    
    * Design: TestDat적용및 css변경
    
    * Design: Css 변경
    
    * Design: Layout 임시 라인 삭제
    
    * None
    
    * Refactor: 주석 삭제
    
    * Docs: ComplainAlarm 기본구조생성
    
    * Design: UI 생성
    
    * None
    
    * Feat: useGetAlarm fetch 적용
    
    * Feat: Route 설정

--- src/4_features/complainAlarm/ui/ComplainAlarm.jsx
+++ src/4_features/complainAlarm/ui/ComplainAlarm.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { S } from "./style";
 import { useGetAlarm } from "../api/useGetAlarm";
 import { DefaultBtn, Icon } from "@shared/ui";
@@ -6,6 +6,7 @@ import { DefaultBtn, Icon } from "@shared/ui";
 export const ComplainAlarm = () => {
     const isAlarm = useGetAlarm();
     const navigate = useNavigate();
+    const location = useLocation();
     const onClickRoute = () => navigate("complain");
 
     return(
@@ -14,6 +15,7 @@ export const ComplainAlarm = () => {
                 <DefaultBtn
                 text="신고 보기"
                 onClick={onClickRoute}
+                type={ location.pathname === "/complain" ? "select" : null}
                 />
                 {isAlarm 
                 ? <S.Alarm>
