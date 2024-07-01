d963867ec1e2ec0f8a50cedd2061c101c91c2141
junik <wkdwnsdlr37@gmail.com>
2024-07-01 10:54:02 +0900

    Refactor: 신고 및 구독 컴포넌트 적용

--- src/3_widgets/aside/ui/Aside.jsx
+++ src/3_widgets/aside/ui/Aside.jsx
@@ -1,36 +1,18 @@
-import { useLocation } from "react-router-dom";
 import { S } from "./style";
-import { createTestData } from "../model/createTestData";
 import { DefaultBtn } from "@shared/ui";
+import { ComplainAlarm  } from "@features/complainAlarm"; 
+import { SubscribeList } from "@features/subscribeList";
 
 export const Aside = () => {
-    const testData = createTestData();
-    const test = useLocation();
-    
   return (
     <>
       <S.Aside>
         <S.BtnList>
           <DefaultBtn text="홈" />
-          <DefaultBtn text="둘러보기" />
-          <DefaultBtn text="신고보기" />
+          <DefaultBtn text="둘러 보기" />
+          <ComplainAlarm/>
         </S.BtnList>
-        
-        <S.SubscribeInfo>
-          <S.SubscribeTitle>구독목록</S.SubscribeTitle>
-
-          <S.SubscribeList>
-          {testData && testData.map((value)=>{
-            return(
-                <S.SubscribeItem key={value.idx}>
-                    <img src={value.profileImg} alt="#" />
-                    <span>{value.nickname}</span>
-                </S.SubscribeItem>
-            );
-          })}
-          </S.SubscribeList>
-          
-        </S.SubscribeInfo>
+        <SubscribeList/>
       </S.Aside>
     </>
   );
