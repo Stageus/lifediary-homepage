8f641a0dfb5de0542e17fe9e23d65103a110660c
junik <wkdwnsdlr37@gmail.com>
2024-07-02 20:35:29 +0900

    Docs: createTestData 생성

--- src/3_widgets/complainContent/model/createTestData.js
+++ src/3_widgets/complainContent/model/createTestData.js
@@ -2,6 +2,7 @@
 export const createTestData = (page)=>{
     const result = [];
     const pageIndex = page ?? 0;
+    const testState = [false,true,null,false,null];
 
     if(!pageIndex){
         for(let i = 0 ; i < 5 ; i++){
@@ -9,8 +10,8 @@ export const createTestData = (page)=>{
                     "idx" : i,
                     "diaryIdx" : i,
                     "nickname" : `첫번째요청 리스트`,
-                    "textContent" : "Test 내용" * i,
-                    "isInvalid" : null,
+                    "textContent" : "Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용",
+                    "isInvalid" : testState[i],
                     "processedAt" : "2024-03-01",
                     "createdAt" : "2024-03-01"
                 },
@@ -22,8 +23,8 @@ export const createTestData = (page)=>{
                     "idx" : i,
                     "diaryIdx" : i,
                     "nickname" : `두번째요청 리스트`,
-                    "textContent" : "Test 내용" * i,
-                    "isInvalid" : null,
+                    "textContent" : "Test 내용",
+                    "isInvalid" : testState[i],
                     "processedAt" : "2024-03-01",
                     "createdAt" : "2024-03-01"
                 },
