77cf73ab602608a4731dd3257f3137a196e19a4c
junik <wkdwnsdlr37@gmail.com>
2024-07-02 12:52:43 +0900

    Refactor: 조건수정

--- src/4_features/grass/lib/grassWrap.js
+++ src/4_features/grass/lib/grassWrap.js
@@ -22,38 +22,29 @@ export const grassWrap = (grassList)=>{
         윤달미포함 오늘기준 366
         윤달포함 오늘기준 367
 
-        # 1년기준 - 윤달 미포함
-        년초 시작 요일과, 년말 끝 요일이 일치하므로 365일
-        ex) 2023-01-01(일) ~ 2023-12-31(일)
+        # 1년기준 - 윤년이 아닐경우 = 365일
+        1년은 365일로써 52주하고 1일,
+        각요일은 52개의 요일을가지고 년을 시작하는 요일에 + 1
+        ex) 2023-01-01(일) ~ 2023-12-31(일) = 일:53 ...
 
-        # 1년기준 + 윤달 포함
-        년초 시작 요일에서 년말 끝 요일에 + 1되어 366일
-        ex) 2024-01-01(월) ~ 2024-12-31(화)
+        # 1년기준 - 윤년일 경우 = 366일
+        1년은 366일로써 52주하고 2일,
+        각요일은 52개의 요일을가지고 년을 시작하는 요일과 다음요일에 + 1
+        ex) 2024-01-01(월) ~ 2024-12-31(화) = 월: 53 화:53
 
-        일
-        월
-        화
-        수
-        목
-        금
-        토
+        # 오늘기준 과거 1년 + 윤년이 포함되지 않은경우 = 366
+        1년은 365일이지만 오늘 날짜를 포함하여 366일,
+        각요일은 52개의 요일을가지고 년을 시작하는 요일과 이전요일에 + 1
+        ex) 2022-07-01(금) ~ 2023-07-01(토) = 금:53 토: 53
 
-        # 오늘기준 과거 1년 + 윤달 포함
-        오늘기준 요일에서 -2되어 367
-        ex) 2024-07-01(월) ~ 2023-07-01(토)
-
-        # 오늘기준 과거 1년 - 윤달 미포함
-        오늘기준 요일에서 -1되어 366
-        ex) 2023-07-01(토) ~ 2022-07-01(금)
+        # 오늘기준 과거 1년 - 윤년이 포함된경우 = 367
+        1년은 366일이지만 오늘 날짜를 포함하여 367일
+        각 요일은 52개의 요일을가지고 년을 시작하는 요일과 이전 요일까지 + 1
+        ex) 2023-07-02(일) ~ 2024-07-02(화) = 일:53 월:53 화: 53 
     
     */
-    const resultWrap = Object.keys(result).map( day => ({[day]:result[day]}));
-    const dayIndex = resultWrap.findLastIndex((day) => day[Object.keys(day)[0]].length === 53);
-    
-    if(grassList.length === 366){
-        for(let i = 0 ; i < dayIndex - 1 ; i++) resultWrap[i][daysOfWeek[i]].unshift(null);
-    }else{
-        for(let i = 0 ; i < dayIndex ; i++) resultWrap[i][daysOfWeek[i]].unshift(null);
-    }
+   const resultWrap = Object.keys(result).map( day => ({[day]:[...result[day]]}));
+   const dayIndex = resultWrap.findIndex((day) => day[Object.keys(day)[0]].length === 53);
+   for(let i = 0 ; i < dayIndex ; i++) resultWrap[i][daysOfWeek[i]].unshift(null);
     return resultWrap;
 };
