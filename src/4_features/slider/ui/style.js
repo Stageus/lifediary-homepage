9a54cc13ab4e0efe96006a90264c79e5ebb27686
Jangmy <108041161+JangIkIk@users.noreply.github.com>
2024-07-02 13:58:37 +0900

    Home page 버튼 Route 추가 (#26)
    * Docs: HomeContent 생성
    
    * Docs: shared img 파일 이동
    
    * Design: UI배치
    
    * Design: Slider css 고정 px 변경
    
    * Design: 고정px 위치변경
    
    * Docs: Home Page파일에 widgets 적용
    
    * Docs: cookie 적용
    
    * Refactor: 주석추가
    
    * Feat: 둘러보기 버튼  Route 추가

--- src/4_features/slider/ui/style.js
+++ src/4_features/slider/ui/style.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 const Slider = styled.div`
+<<<<<<< HEAD
+    /* max-width: 1080px; */
+=======
+>>>>>>> 1200f09b09921e3920e0b9b3110a21c62f708e7d
     width: 100%;
     display: flex;
     justify-content: center;
