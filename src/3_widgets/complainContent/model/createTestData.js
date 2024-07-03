d1c945b007a23b338e7d930b2d1a6f0a9da68ca3
junik <wkdwnsdlr37@gmail.com>
2024-07-03 09:52:37 +0900

    Refactor: createTestData 조건 수정

--- src/3_widgets/complainContent/model/createTestData.js
+++ src/3_widgets/complainContent/model/createTestData.js
@@ -1,15 +1,13 @@
 
 export const createTestData = (page)=>{
     const result = [];
-    const pageIndex = page ?? 0;
     const testState = [false,true,null,false,null];
 
-    if(!pageIndex){
         for(let i = 0 ; i < 5 ; i++){
             result.push({
                     "idx" : i,
                     "diaryIdx" : i,
-                    "nickname" : `첫번째요청 리스트`,
+                    "nickname" : `${page}첫번째요청 리스트`,
                     "textContent" : "Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용Test 내용",
                     "isInvalid" : testState[i],
                     "processedAt" : "2024-03-01",
@@ -17,19 +15,5 @@ export const createTestData = (page)=>{
                 },
             );
         }
-    }else{
-        for(let i = 0 ; i < 5 ; i++){
-            result.push({
-                    "idx" : i,
-                    "diaryIdx" : i,
-                    "nickname" : `두번째요청 리스트`,
-                    "textContent" : "Test 내용",
-                    "isInvalid" : testState[i],
-                    "processedAt" : "2024-03-01",
-                    "createdAt" : "2024-03-01"
-                },
-            );
-        }
-    }
     return result;
 }
