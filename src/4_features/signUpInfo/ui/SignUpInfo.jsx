6f487e260b86d2b10913b65e025abca6b355ad46
mangkong1 <xogud5053@hanmail.net>
2024-07-03 13:56:45 +0900

    Refactor: SignUpInfo 이벤트 함수 별도의 파일로 분리

--- src/4_features/signUpInfo/ui/SignUpInfo.jsx
+++ src/4_features/signUpInfo/ui/SignUpInfo.jsx
@@ -1,54 +1,27 @@
 import React, { useRef, useState, useCallback } from "react";
 
 import { S } from "./style";
-import { useFetch } from "@shared/hook/useFetch";
 import { DefaultBtn } from "@shared/ui/defaultBtn/DefaultBtn";
 import { TextInput } from "@shared/ui/textInput/TextInput";
 import Profile from "@shared/assets/imges/profile.png";
+import { handleChangeProfileImg, handleChangeImgBase } from "../lib/changeProfileImg";
+import { handleCheckInputValue } from "../lib/checkInputValueLength";
+import { useFetch } from "@shared/hook/useFetch";
 
 export const SignUpInfo = () => {
   const [inputType, setInputType] = useState("");
   const [nickname, setNickname] = useState("");
   const [oauthGoogleId, setOauthGoogleId] = useState("1");
   const [profileImg, setProfileImg] = useState(Profile);
-  const imageInput = useRef(null);
+  const imageInput = useRef(null); // imageInput이라는 참조 객체 생성
   const [data, errorStatus, baseFetch] = useFetch();
 
-  const handleChangeProfileImg = useCallback(() => {
-    imageInput.current.click();
-  }, []);
-
-  const handleImageChange = (e) => {
-    e.preventDefault();
-    let file = e.target.files[0];
-    let reader = new FileReader();
-
-    reader.onloadend = () => {
-      setProfileImg(reader.result);
-    };
-
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-  };
-
-  const handleChangeValue = (e) => {
-    const inputValue = e.target.value;
-    setNickname(inputValue);
-
-    if (inputValue.length < 5) {
-      setInputType("error");
-    } else {
-      setInputType("");
-    }
-  };
+  const handleChangeProfileImgCall = handleChangeProfileImg(imageInput);
+  const handleChangeImgBaseCall = handleChangeImgBase(setProfileImg);
+  const handleCheckInputValueCall = handleCheckInputValue(setNickname, setInputType);
 
   const handleUploadInfo = () => {
     console.log(profileImg, nickname, oauthGoogleId);
-    // const formData = new FormData();
-    // formData.append("profileImg", profileImg);
-    // formData.append("nickname", nickname);
-    // formData.append("oauthGoogleId", oauthGoogleId);
 
     const response = baseFetch("account", {
       method: "POST",
@@ -66,14 +39,15 @@ export const SignUpInfo = () => {
     <>
       <S.SignUpInfoContainer>
         <S.SignUpInfoProfileImgContainer>
-          <input type="file" multiple hidden ref={imageInput} onChange={handleImageChange} />
-          {profileImg && <S.SignUPInfoProfileImg src={profileImg} onClick={handleChangeProfileImg} />}
+          {/* ref속성으로 imageInput을 설정해 입력요소의 참조를 imageInput.current에 저장 */}
+          <input type="file" hidden ref={imageInput} onChange={handleChangeImgBaseCall} />
+          {profileImg && <S.SignUPInfoProfileImg src={profileImg} onClick={handleChangeProfileImgCall} />}
           <S.SignUpInfoMessage>프로필을 선택해 주세요(jpg, jpeg, gif, png)</S.SignUpInfoMessage>
         </S.SignUpInfoProfileImgContainer>
         <S.SignUpInfoNicknameContainer>
           <S.SignUpInfoNicknameLabel>닉네임</S.SignUpInfoNicknameLabel>
           <S.SignUpInfoNicknameInputContainer>
-            <TextInput type={inputType} placeholder="닉네임을 입력해주세요(최대 20자)" onBlur={handleChangeValue} onChange={handleChangeValue}>
+            <TextInput type={inputType} placeholder="닉네임을 입력해주세요(최대 20자)" onBlur={handleCheckInputValueCall} onChange={handleCheckInputValueCall}>
               {nickname}
             </TextInput>
             {inputType === "error" && <S.SignUpInfoMessage>5자 이상 입력해주세요.</S.SignUpInfoMessage>}
