0ad742dfefb5c0575a8df05adf011eb80faab7d0
mangkong1 <xogud5053@hanmail.net>
2024-07-08 02:11:46 +0900

    Refactor: 토큰 불러오는 기능 파일 분리

--- src/4_features/googleLogin/ui/GoogleLogin.jsx
+++ src/4_features/googleLogin/ui/GoogleLogin.jsx
@@ -1,39 +1,16 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useGoogleLogin } from "@react-oauth/google";
-
 import { S } from "./style";
 import { useGetRedirectUrl } from "../api/useGetRedirectUrl";
-import { useFetch } from "@shared/hook/useFetch";
+import { usePostToken } from "./usePostToken"; // 가정한 경로
 
 export const GoogleLoginBtn = () => {
   const [redirectData] = useGetRedirectUrl();
   const [response, setResponse] = useState(null);
-  const [responseData, errorStatus, baseFetch] = useFetch();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (response) {
-        const { credential } = response;
-        await baseFetch(redirectData.result.redirectUrl, {
-          method: "POST",
-          headers: {
-            token: credential,
-          },
-        });
-
-        if (errorStatus) {
-          console.log(errorStatus);
-        } else {
-          console.log(responseData);
-          navigate("/home");
-        }
-      }
-    };
-
-    fetchData();
-  }, [response, redirectData, baseFetch, errorStatus, navigate, responseData]); //의존성 배열에 들어갈 목록에 관해 아직 확실하지 않음
+  usePostToken(response, redirectData, navigate);
 
   const handleLogin = useGoogleLogin({
     onSuccess: (response) => setResponse(response),
