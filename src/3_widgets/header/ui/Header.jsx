45af9066a7781d4c962b36a6d27a89587f6ce0a6
mangkong1 <xogud5053@hanmail.net>
2024-07-09 03:48:16 +0900

    Feat: 로그아웃 기능 생성

--- src/3_widgets/header/ui/Header.jsx
+++ src/3_widgets/header/ui/Header.jsx
@@ -6,11 +6,13 @@ import DefaultProfile from "@shared/assets/imges/profile.png";
 import { DefaultBtn } from "@shared/ui/defaultBtn/DefaultBtn";
 import { navigatePage } from "../lib/navigatePage";
 import { useGetProfileImage } from "../api/useGetProfileImg";
+import { useLogout } from "../lib/useLogout";
 
 export const Header = () => {
   const [profileImg, setProfileImg] = useState(DefaultProfile);
   const [navigateMyProfile, navigateDiaryCreate, navigateHome] = navigatePage();
   const myProfileImg = useGetProfileImage();
+  const logout = useLogout;
 
   useEffect(() => {
     if (myProfileImg) {
@@ -27,7 +29,7 @@ export const Header = () => {
           {profileImg && <S.ProfileImg src={profileImg} onClick={navigateMyProfile} />}
           <DefaultBtn text="일기업로드" onClick={navigateDiaryCreate} />
           <DefaultBtn text="알림" />
-          <DefaultBtn text="로그아웃" />
+          <DefaultBtn text="로그아웃" onClick={logout} />
         </S.RightElemContainer>
       </S.HeaderContainer>
     </>
