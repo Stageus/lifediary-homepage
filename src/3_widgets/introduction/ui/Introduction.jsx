b7cea2a91e1d192410ea1953cd0cd780dfeb52b0
junik <wkdwnsdlr37@gmail.com>
2024-07-09 07:34:03 +0900

    Docs: HomeContent 파일분할

--- src/3_widgets/introduction/ui/Introduction.jsx
+++ src/3_widgets/introduction/ui/Introduction.jsx
@@ -1,8 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { S } from "./style";
+import defaultDiary1 from "../assets/defaultDiary1.png";
+import description from "../assets/description.png";
+import { DefaultBtn } from "@shared/ui";
+
 
 export const Introduction = ()=>{
+    const navigate = useNavigate();
+    const onClickRoute = () => navigate("diary");
     return(
         <>
-            
+        <S.Introduction>
+                    <S.SectionFirst $img={description}>
+                        <S.SectionFirstContent>
+                            <h1>인생일기</h1>
+                            <p>{`하루일상을\n일기로 관리 해보세요\n\n꾸준한 기록으로 과거를 기록하세요.\n자신의 과거를 기억하는 좋은방법!\n그날의 기분은 색상으로 간단하게 알 수 있어요`}</p>
+                        </S.SectionFirstContent>
+                        <div>
+                            <DefaultBtn
+                            text="일기둘러보기"
+                            type="select"
+                            onClick={onClickRoute}
+                            />
+                        </div>
+                    </S.SectionFirst>
+                    <S.SectionSecond $img={defaultDiary1}/>
+                </S.Introduction>
         </>
     );
 }
