db12f9a047a06a5dfaee85aefae25b95dd34c128
mangkong1 <xogud5053@hanmail.net>
2024-07-08 18:36:09 +0900

    Feat: Header ui 생성

--- src/3_widgets/header/ui/Header.jsx
+++ src/3_widgets/header/ui/Header.jsx
@@ -1,8 +1,25 @@
-import "./style";
-export const Header = ()=>{
-    return(
-        <>
-            <header>헤더</header>
-        </>
-    );
-}
+import { useState } from "react";
+
+import { S } from "./style";
+import { TagInput } from "@shared/ui/tagInput/TagInput";
+import Profile from "@shared/assets/imges/profile.png";
+import { DefaultBtn } from "@shared/ui/defaultBtn/DefaultBtn";
+
+export const Header = () => {
+  const [profileImg, setProfileImg] = useState(Profile);
+
+  return (
+    <>
+      <S.HeaderContainer>
+        <S.Logo />
+        <TagInput />
+        <S.RightElemContainer>
+          {profileImg && <S.ProfileImg src={profileImg} />}
+          <DefaultBtn text="일기업로드" />
+          <DefaultBtn text="알림" />
+          <DefaultBtn text="로그아웃" />
+        </S.RightElemContainer>
+      </S.HeaderContainer>
+    </>
+  );
+};
