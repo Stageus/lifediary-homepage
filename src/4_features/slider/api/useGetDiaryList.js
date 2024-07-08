ea14b3913c3358bfbf0a76ce76d8bce9db911bc3
junik <wkdwnsdlr37@gmail.com>
2024-07-09 08:11:01 +0900

    Docs: sliceArray 함수명 변경

--- src/4_features/slider/api/useGetDiaryList.js
+++ src/4_features/slider/api/useGetDiaryList.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { sliceArray } from "../lib/sliceArray";
+import { sliceDiaryCount } from "../lib/sliceDiaryCount";
 import { useFetch } from "@shared/hook";
 
 // 임시데이터
@@ -16,18 +16,18 @@ export const useGetDiaryList = () =>{
     const getDiaryList = ()=>{
         if(!page){
             // 임시데이터
-            setDiaryList(sliceArray(createTestData(), 5));
+            setDiaryList(sliceDiaryCount(createTestData(), 5));
             // 임시주석
             // baseFetch("diary/home");
-            // setDiaryList(sliceArray(fetchData, 5));
+            // setDiaryList(sliceDiaryCount(fetchData, 5));
             return;
         }
 
         // 임시데이터
-        setDiaryList([...diaryList,...sliceArray(createTestData(page), 5)]);
+        setDiaryList([...diaryList,...sliceDiaryCount(createTestData(page), 5)]);
         // 임시주석
         // baseFetch(`diary/home?page=${page}`);
-        // setDiaryList([...diaryList, ...sliceArray(fetchData, 5)]);
+        // setDiaryList([...diaryList, ...sliceDiaryCount(fetchData, 5)]);
     }
 
     useEffect(()=>{
