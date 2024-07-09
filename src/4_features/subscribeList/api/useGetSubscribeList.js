118612b901fa623b3350548564a7fa13bd27bcbb
junik <wkdwnsdlr37@gmail.com>
2024-07-09 10:11:15 +0900

    None

--- src/4_features/subscribeList/api/useGetSubscribeList.js
+++ src/4_features/subscribeList/api/useGetSubscribeList.js
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
-import { createTestData } from "../model/createTestData";
+import { createTestData } from "../service/createTestData";
 import { useFetch, useCookie, useSubscribe } from "@shared/hook";
 
 
 export const useGetSubscribeList = ()=>{
     const [subscribeList, setSubscribeList] = useState([]);
-    const [fetchData, errorStatus, baseFetch] = useFetch();
+    const [fetchData, status, baseFetch] = useFetch();
     const { handleGetCookie } = useCookie();
     const isSubscribe = useSubscribe( state => state.value);
     const getSubscribeList = (page)=>{
-        const pageIndex = page ?? 0;
+        const pageIndex = page ?? 1;
         
         // 임시데이터
-        if(!pageIndex){
+        if(pageIndex === 1){
             setSubscribeList(createTestData());
         }else{
             setSubscribeList([...subscribeList,...createTestData(page)]);
@@ -27,11 +27,11 @@ export const useGetSubscribeList = ()=>{
     useEffect(()=>{
         getSubscribeList();
 
-        if(errorStatus){
-            return "에러바운더리 대기";
+        if(status === 200){
+            return setSubscribeList(fetchData);
         }
 
-    },[isSubscribe,errorStatus])
+    },[isSubscribe,status])
 
     return [subscribeList, getSubscribeList];
 }
