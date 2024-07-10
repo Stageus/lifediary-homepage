0526fe9f71c284f9d752ab52b82ff80f12521f15
mangkong1 <xogud5053@hanmail.net>
2024-07-10 09:40:34 +0900

    Bug: 파일 생성 오류로 인한 revert 수행

--- src/3_widgets/grass/index 2.js
+++
@@ -1,1 +0,0 @@
-export { Grass } from "./ui/Grass";
--- src/3_widgets/header/api/useGetProfileImg.js
+++ src/3_widgets/header/api/useGetProfileImg.js
@@ -1,30 +1,33 @@
 import { useState, useEffect } from "react";
-
 import { useFetch } from "@shared/hook/useFetch";
-import { useCookie } from "@shared/hook/useCookie";
 
 export const useGetProfileImage = () => {
   const [myProfileImg, setMyProfileImg] = useState(null);
-  const [profileImgData, errorStatus, baseFetch] = useFetch();
-  const { handleGetCookie } = useCookie();
+  const [, , baseFetch] = useFetch();
 
   useEffect(() => {
-    baseFetch("account", {}, handleGetCookie());
-    setMyProfileImg(profileImgData);
+    const fetchProfileImg = () => {
+      try {
+        const response = baseFetch("account", {
+          method: "GET",
+          headers: {
+            token: credential,
+          },
+        });
 
-    if (errorStatus) {
-      console.log("Error: ", errorStatus);
-    }
+        const data = response.json();
+        if (data.message === "success") {
+          setMyProfileImg(data.result.profileImg);
+        } else {
+          console.error("Profile image fetch failed: ", data);
+        }
+      } catch (error) {
+        console.error("Error: ", error);
+      }
+    };
+
+    fetchProfileImg();
   }, [baseFetch]);
-
-  //임시데이터로 테스트하는 코드
-  // useEffect(() => {
-  //   setMyProfileImg("https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg");
-
-  //   if (errorStatus) {
-  //     console.log("Error: ", errorStatus);
-  //   }
-  // }, []);
 
   return myProfileImg;
 };
