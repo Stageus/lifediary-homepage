54b435c1ed6f9503b79eb2ea5403c393f164e39e
mangkong1 <xogud5053@hanmail.net>
2024-07-11 06:05:55 +0900

    Refactor: 외부 클릭시 색상 파레트 사라지는 기능 구현

--- src/4_features/createGrass/ui/CreateGrass.jsx
+++ src/4_features/createGrass/ui/CreateGrass.jsx
@@ -1,3 +1,5 @@
+import { useState, useRef, useEffect } from "react";
+
 import { ChromePicker } from "react-color";
 
 import { S } from "./style";
@@ -6,6 +8,7 @@ import { DefaultBtn } from "@shared/ui";
 export const CreateGrass = () => {
   const [color, setColor] = useState("");
   const [showPicker, setShowPicker] = useState(false);
+  const pickerRef = useRef();
 
   const handleColorChange = (color) => {
     setColor(color.hex);
@@ -15,6 +18,22 @@ export const CreateGrass = () => {
     const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     setColor(randomColor);
   };
+
+  useEffect(() => {
+    // 외부 클릭 감지 함수
+    const handleClickOutside = (event) => {
+      if (pickerRef.current && !pickerRef.current.contains(event.target)) {
+        setShowPicker(false);
+      }
+    };
+
+    // 문서에 클릭 이벤트 리스너 추가
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // 컴포넌트 언마운트 시 이벤트 리스너 제거
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <>
