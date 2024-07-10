e833c931c23d5062b928011ee9e3f18cc3f8e399
mangkong1 <xogud5053@hanmail.net>
2024-07-10 14:06:02 +0900

    Design: DiaryCreateInfo 요소 배치를 위한 css수정

--- src/4_features/diaryCreateInfo/ui/DiaryCreateInfo.jsx
+++ src/4_features/diaryCreateInfo/ui/DiaryCreateInfo.jsx
@@ -6,29 +6,39 @@ export const DiaryCreateInfo = () => {
     <>
       <S.DiaryCreateContainer>
         <S.ContentContainer>
-          <p>내용</p>
+          <S.ContentNameContainer>내용</S.ContentNameContainer>
           <S.textContent />
         </S.ContentContainer>
         <S.ContentContainer>
-          <p>이미지</p>
-          <DefaultBtn text="선택(최대 3개)" />
+          <S.ContentNameContainer>이미지</S.ContentNameContainer>
+          <div>
+            <DefaultBtn text="선택(최대 3개)" />
+          </div>
         </S.ContentContainer>
         <S.ContentContainer>
-          <p>태그</p>
+          <S.ContentNameContainer>태그</S.ContentNameContainer>
           <TagInput />
         </S.ContentContainer>
         <S.ContentContainer>
-          <p>잔디(필수)</p>
-          <DefaultBtn text="선택" />
-          <DefaultBtn text="랜덤배정" />
+          <S.ContentNameContainer>잔디(필수)</S.ContentNameContainer>
+          <div>
+            <DefaultBtn text="선택" />
+          </div>
+          <div>
+            <DefaultBtn text="랜덤배정" />
+          </div>
         </S.ContentContainer>
         <S.ContentContainer>
-          <p>공개</p>
-          <Icon type="calender" />
+          <S.ContentNameContainer>공개</S.ContentNameContainer>
+          <Icon type="calendar" />
         </S.ContentContainer>
         <S.BtnContainer>
-          <DefaultBtn text="작성" />
-          <DefaultBtn text="취소" />
+          <div>
+            <DefaultBtn text="작성" type="disabled" />
+          </div>
+          <div>
+            <DefaultBtn text="취소" />
+          </div>
         </S.BtnContainer>
       </S.DiaryCreateContainer>
       ;
--- src/4_features/diaryCreateInfo/ui/style.js
+++ src/4_features/diaryCreateInfo/ui/style.js
@@ -10,24 +10,34 @@ const DiaryCreateContainer = styled.div`
 const ContentContainer = styled.div`
   display: flex;
   align-items: center;
+  gap: 20px;
 `;
 
-const textContent = styled.div`
+const ContentNameContainer = styled.p`
+  white-space: nowrap;
+  display: flex;
+  width: 80px;
+`;
+
+const textContent = styled.textarea`
   width: 600px;
   height: 300px;
   border: ${({ theme }) => `3px solid ${theme.major}`};
   border-radius: 10px;
+  padding: 20px;
 `;
 
 const BtnContainer = styled.div`
   display: flex;
   justify-content: center;
   gap: 40px;
+  margin-top: 80px;
 `;
 
 export const S = {
   DiaryCreateContainer,
   ContentContainer,
+  ContentNameContainer,
   textContent,
   BtnContainer,
 };
