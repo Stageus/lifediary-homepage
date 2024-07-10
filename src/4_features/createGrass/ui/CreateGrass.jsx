5b336fcf5046a19574b44220106eb23e0cab4bde
mangkong1 <xogud5053@hanmail.net>
2024-07-11 06:05:06 +0900

    Feat: CreateGrass 색상 파레트 생성, 전체 배치

--- src/4_features/createGrass/ui/CreateGrass.jsx
+++ src/4_features/createGrass/ui/CreateGrass.jsx
@@ -1,16 +1,41 @@
+import { ChromePicker } from "react-color";
+
 import { S } from "./style";
 import { DefaultBtn } from "@shared/ui";
 
 export const CreateGrass = () => {
+  const [color, setColor] = useState("");
+  const [showPicker, setShowPicker] = useState(false);
+
+  const handleColorChange = (color) => {
+    setColor(color.hex);
+  };
+
+  const handleRandomColor = () => {
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    setColor(randomColor);
+  };
+
   return (
     <>
       <S.ContentContainer>
-        <S.ContentNameContainer>잔디(필수)</S.ContentNameContainer>
-        <div>
-          <DefaultBtn text="선택" />
-        </div>
-        <div>
-          <DefaultBtn text="랜덤배정" />
+        <S.NameAndBtnContainer>
+          <S.ContentNameContainer>
+            잔디<S.NameHighlight>(필수)</S.NameHighlight>
+          </S.ContentNameContainer>
+          {color && <S.ColorPreview color={color} />}
+          <div>
+            <DefaultBtn text="랜덤배정" onClick={handleRandomColor} />
+          </div>
+        </S.NameAndBtnContainer>
+        <div ref={pickerRef}>
+          {showPicker ? (
+            <ChromePicker color={color} onChange={handleColorChange} />
+          ) : (
+            <div>
+              <DefaultBtn text="선택" onClick={() => setShowPicker(true)} />
+            </div>
+          )}
         </div>
       </S.ContentContainer>
     </>
--- src/4_features/createGrass/ui/style.js
+++ src/4_features/createGrass/ui/style.js
@@ -2,6 +2,12 @@ import styled from "styled-components";
 
 const ContentContainer = styled.div`
   display: flex;
+  align-items: flex-start;
+  gap: 20px;
+`;
+
+const NameAndBtnContainer = styled.div`
+  display: flex;
   align-items: center;
   gap: 20px;
 `;
@@ -12,7 +18,21 @@ const ContentNameContainer = styled.p`
   width: 80px;
 `;
 
+const NameHighlight = styled.span`
+  color: ${({ theme }) => theme.highlight};
+`;
+
+const ColorPreview = styled.div`
+  width: 30px;
+  height: 30px;
+  background-color: ${({ color }) => color};
+  margin-right: 10px;
+`;
+
 export const S = {
   ContentContainer,
+  NameAndBtnContainer,
   ContentNameContainer,
+  NameHighlight,
+  ColorPreview,
 };
