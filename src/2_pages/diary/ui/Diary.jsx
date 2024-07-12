e4b6531f3babda6fae188a64728a357723257f74
junik <wkdwnsdlr37@gmail.com>
2024-07-12 11:10:05 +0900

    Design: diaryPage Css 변경

--- src/2_pages/diary/ui/Diary.jsx
+++ src/2_pages/diary/ui/Diary.jsx
@@ -1,20 +1,77 @@
+import { useEffect, useRef } from "react";
 import { S } from "./style";
 import { useGetDiaryList } from "../api/useGetDiaryList";
 import { DiaryInfo } from "@widgets/diaryInfo";
+import { SubscribeBtn } from "@features/subscribeBtn";
+import { DefaultBtn } from "@shared/ui";
 
 export const Diary = () => {
-    const [diaryList, nextPage] = useGetDiaryList();
+  const [diaryList, nextPage] = useGetDiaryList();
+
+//   const scrollRef = useRef(null);
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       if (scrollRef.current) {
+//         const position = scrollRef.current.scrollTop;
+//         console.log(position);
+//       }
+//     };
+
+//     const scrollElement = scrollRef.current;
+//     if (scrollElement) {
+//       scrollElement.addEventListener("scroll", handleScroll);
+//     }
+
+//     return () => {
+//       if (scrollElement) {
+//         scrollElement.removeEventListener("scroll", handleScroll);
+//       }
+//     };
+//   }, []);
+//   console.log(scrollRef);
+
   return (
     <>
       <S.Diary>
         {diaryList &&
           diaryList.map((diary) => {
-            console.log(diary)
             return (
-              <DiaryInfo
-              key={diary.idx}
-              diary={diary}
-              />
+              <S.ScrollItem key={diary.idx}>
+                <S.DiaryHeader>
+                  <S.DiaryHeaderContainer>
+                    <S.UserImg>
+                      <img src={diary.profileImg} alt="#" />
+                    </S.UserImg>
+
+                    <S.UserName>
+                      <span>{diary.nickname}</span>
+                    </S.UserName>
+
+                    <S.DiaryCt>
+                      <span>{diary.createdAt}</span>
+                    </S.DiaryCt>
+
+                    <S.DiarySubscribe>
+                      <SubscribeBtn
+                        isSubscribed={diary.isSubscribed}
+                        accountIdx={diary.idx}
+                      />
+                    </S.DiarySubscribe>
+                  </S.DiaryHeaderContainer>
+                  <S.DiaryEditor>
+                    <div>
+                      <DefaultBtn text="일기수정" />
+                    </div>
+                    <div>
+                      <DefaultBtn text="일기삭제" />
+                    </div>
+                  </S.DiaryEditor>
+                </S.DiaryHeader>
+
+                <S.DiaryInfoContainer>
+                    <DiaryInfo key={diary.idx} diary={diary} />
+                </S.DiaryInfoContainer>
+              </S.ScrollItem>
             );
           })}
       </S.Diary>
--- src/2_pages/diary/ui/style.js
+++ src/2_pages/diary/ui/style.js
@@ -1,12 +1,86 @@
 import styled from "styled-components";
 
+// 리스트 부모
 const Diary = styled.div`
     height: calc(100vh - 80px);
     scroll-snap-type: y mandatory;
     overflow-y: scroll;
+    display: flex;
+    flex-direction: column;
 `; 
+
+// 리스트
+const ScrollItem = styled.div`
+    scroll-snap-align: center;
+    scroll-snap-stop: always;
+    flex: 1 0 100%;
+    padding: 20px 15px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    overflow-y: visible;
+`;
+
+// 리스트 헤더
+const DiaryHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const DiaryHeaderContainer = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    
+`;
+
+const UserImg = styled.div`
+    width: 34px;
+    height: 34px;
+    display: flex;
+    border-radius: 50%;
+    & > img{
+        width: 100%;
+    }
+`;
+
+const UserName = styled.div`
+    font-size: ${({theme})=> theme.fontSize.base};
+    color: ${({theme}) => theme.black};
+`;
+
+const DiaryCt = styled.div`
+    font-size: ${({theme})=> theme.fontSize.medium};
+    color: ${({theme}) => theme.black};
+`;
+
+const DiarySubscribe = styled.div`
+    width: 80px;
+`;
+
+const DiaryEditor = styled.div`
+    display: flex;
+    gap: 10px;
+`;
+// -----------------------------
+
+const DiaryInfoContainer = styled.div`
+    flex-grow: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
 
 export const S = {
     Diary,
+    ScrollItem,
+    DiaryHeader,
+    DiaryHeaderContainer,
+    UserImg,
+    UserName,
+    DiaryCt,
+    DiarySubscribe,
+    DiaryEditor,
+    DiaryInfoContainer,
 }
