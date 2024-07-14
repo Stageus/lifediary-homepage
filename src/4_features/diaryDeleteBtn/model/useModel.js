44a4de6941cec0a555db06b99e94be9ae5a6764a
junik <wkdwnsdlr37@gmail.com>
2024-07-15 08:49:38 +0900

    Feat: useModel 작성

--- src/4_features/diaryDeleteBtn/model/useModel.js
+++ src/4_features/diaryDeleteBtn/model/useModel.js
@@ -1,3 +1,13 @@
-export const useModel = () => {
-    return "";
+import { useDeleteDiary } from "../api/useDeleteDiary";
+
+export const useModel = ( diaryIdx ) => {
+    const [ deleteDiary ] = useDeleteDiary();
+
+    const onClickConfirm = () => {
+        const ConFirm = confirm("해당 일기를 삭제하시 겠습니까?");
+        if ( !ConFirm ) return;
+        deleteDiary(diaryIdx);
+    }
+
+    return { onClickConfirm };
 }
