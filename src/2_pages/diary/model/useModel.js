55ac08bcc64d139156b3e9b51709e01719b0a747
junik <wkdwnsdlr37@gmail.com>
2024-07-14 13:51:27 +0900

    Feat: useModel nextApi 조건 추가

--- src/2_pages/diary/model/useModel.js
+++ src/2_pages/diary/model/useModel.js
@@ -1,9 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import { useGetDiaryList } from "../api/useGetDiaryList";
 import { parseTime } from "@shared/util";
+import { useScroll } from "@shared/hook";
+import { useEffect } from "react";
 
+
+/*
+    useParams로 판단하여 diaryidx가 없다면
+    getDiaryList() 로요청하고
+    
+    diaryIdx가 있다면
+    getDiaryList(diaryidx) 요청하며
+
+    lastScroll이 변화했다면 이름감지하여,
+    nextPage()를 호출한다.
+    nextPage로 호출하는것은 diaryIdx가 없이 요청되어야하고, 랜덤이다
+*/
 export const useModel = ()=>{
     const [diaryList, nextPage] = useGetDiaryList();
+    const [lastScroll, scrollRef, onScrollNext, onScrollReset] = useScroll();
     const navigate = useNavigate();
 
     const onClickRoute = (isMine, accountIdx) => {
@@ -23,36 +38,38 @@ export const useModel = ()=>{
         navigate(`/diaryUpload/${diaryIdx}`);
     };
 
+    useEffect(()=>{
+        if(lastScroll){
+            nextPage();
+            onScrollReset();
+        }
 
-    return {diaryList, onClickRoute, onClickTimeRoute};
+    },[lastScroll])
+
+
+    return {diaryList, onClickRoute, onClickTimeRoute, scrollRef, onScrollNext};
 }
 
 
+
+
+
+
+
 /*
-    현재의 문제점
-    메인페이지에서는 자신의 리스트가 보이지 않는다
-    자신의 일기를 수정 or 삭제를 하려면 메인페이지에서 수정과 삭제 버튼을 출력해야하는데
-    그렇기에 잔디요소 클릭 or 마이페이지의 내일기들을 클릭해서 올수가 있다
-    수정같은경우는 하루가 지난시점에서 비활성화를 하거나, 숨기면 문제가 되지않음
-    삭제또한 문제가 되지 않는다.
-    그렇다면 요소를 클릭후 스크롤하면? 바로 다음 일기리스트를 보여준다?
-    문제가 안될까 ?
+    const [lastScroll, scrollRef, onScrollNext, onScrollReset] = useScroll();
 
-    문제가 될것같다.
+  // useEffect(()=>{
+  //   if (lastScroll && scrollRef.current) {
+  //     scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+  //   }
+  // },[diaryList])
 
-    자신의 일기를 볼때가 문제가된다.
+  // if(lastScroll){
+  //   console.log("마지막에 도달했으니 한번더 리스트요청")
+  //   nextPage();
+  //   onScrollReset();
+  // }
+  // // console.log("무한루프 방지");
 
-    1. 프로필을 클릭하면 자신의 페이지로 가야하고,
-    2. 구독버튼은 숨겨야한다
-    3. 일기수정 과  일기삭제 버튼을 보여주어야한다.
-
-    이때 해당 일기가 자신인것을 판단해야하는데
-
-    어차피 메인페이지에서는  특정일기를 포함한 리스트를 서버에서 반환해주는데
-
-    이 특정일기를 나의 일기인지를 판단하려면
-    특정일기 리스트는 유저의 idx가 필요함
-    그래야 내Token정보에 요청하여 나의 idx
-    그리고 일기의 idx 여부에 따라 구독버튼, 수정,삭제 여부를 표시해야한다
-
-*/
+*/ 
