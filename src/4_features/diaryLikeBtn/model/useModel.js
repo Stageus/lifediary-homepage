674c768d15b78afdcd70b2ba14fe816f8cb9f2f0
junik <wkdwnsdlr37@gmail.com>
2024-07-15 10:59:57 +0900

    Feat: useModel 작성

--- src/4_features/diaryLikeBtn/model/useModel.js
+++ src/4_features/diaryLikeBtn/model/useModel.js
@@ -1,4 +1,10 @@
-export const useModel = () => {
-    
-    return {};
+import { usePostDiaryLike } from "../api/usePostDiaryLike";
+
+export const useModel = ( props ) => {
+    const { diaryIdx, likeCnt, isLiked } = props;
+    const [ isLike, likeCount, postDiaryLike ] = usePostDiaryLike( isLiked, likeCnt );
+
+    const onClickLike = () => postDiaryLike( diaryIdx );
+
+    return {isLike, likeCount, onClickLike};
 }
