4d8c4ad5f3e248ef1b45090656fb2cf2dc0a2df6
junik <wkdwnsdlr37@gmail.com>
2024-07-15 11:00:22 +0900

    Design UI배치 및 로직적용

--- src/4_features/diaryLikeBtn/ui/DiaryLikeBtn.jsx
+++ src/4_features/diaryLikeBtn/ui/DiaryLikeBtn.jsx
@@ -1,11 +1,23 @@
+import { S } from "./style";
 import { useModel } from "../model/useModel";
+import { Icon } from "@shared/ui";
 
-export const DiaryLikeBtn = ( diaryLike ) => {
-    const {} = useModel();
-    
+export const DiaryLikeBtn = ( props ) => {
+    const { isLike, likeCount, onClickLike } = useModel(props);
+
     return(
         <>
-
+            <S.DiaryLikeBtn $isLike={isLike} onClick={onClickLike}>
+                <div>
+                    <Icon
+                    type="like"
+                    color={isLike ? "#F1F1F1" : "#FF6767" }
+                    />
+                </div>
+                <div>
+                    {`${likeCount}개`}
+                </div>
+            </S.DiaryLikeBtn>
         </>
     );
 }
