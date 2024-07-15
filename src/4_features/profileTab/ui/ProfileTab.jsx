727f51dcaf871cd21dbcef0044f850e179df4956
mangkong1 <xogud5053@hanmail.net>
2024-07-16 02:03:14 +0900

    Feat: 탭 클릭시 디자인 변하는 이벤트 구현

--- src/4_features/profileTab/ui/ProfileTab.jsx
+++ src/4_features/profileTab/ui/ProfileTab.jsx
@@ -1,16 +1,19 @@
+import { useState } from "react";
+
 import { S } from "./style.js";
-import { DefaultBtn } from "@shared/ui";
 
 export const ProfileTab = () => {
+  const [selectedTab, setSelectedTab] = useState("myDiaryTab");
+
+  const handleTabBtnClick = (e) => {
+    setSelectedTab(e);
+  };
+
   return (
     <>
       <S.ProfileTabContainer>
-        <S.TabBtnContainer>
-          <DefaultBtn text="내 일기" />
-        </S.TabBtnContainer>
-        <S.TabBtnContainer>
-          <DefaultBtn text="좋아요 표시한 일기" />
-        </S.TabBtnContainer>
+        <S.TabBtnContainer onClick={() => handleTabBtnClick("myDiaryTab")}>{selectedTab === "myDiaryTab" ? <S.ActiveTabBtn>내 일기</S.ActiveTabBtn> : <S.DefaultTabBtn>내 일기</S.DefaultTabBtn>}</S.TabBtnContainer>
+        <S.TabBtnContainer onClick={() => handleTabBtnClick("likeDiaryTab")}>{selectedTab === "likeDiaryTab" ? <S.ActiveTabBtn>좋아요 표시한 일기</S.ActiveTabBtn> : <S.DefaultTabBtn>좋아요 표시한 일기</S.DefaultTabBtn>}</S.TabBtnContainer>
       </S.ProfileTabContainer>
     </>
   );
--- src/4_features/profileTab/ui/style.js
+++ src/4_features/profileTab/ui/style.js
@@ -10,7 +10,31 @@ const TabBtnContainer = styled.div`
   margin-left: 20px;
 `;
 
+const ActiveTabBtn = styled.div`
+  color: ${({ theme }) => theme.major};
+  background-color: ${({ theme }) => theme.highlight};
+  font-size: ${({ theme }) => theme.fontSize.large};
+  border: ${({ theme }) => `1px solid ${theme.highlight}`};
+  border-bottom: none;
+  border-radius: 10px 10px 0 0;
+  padding: 8px 16px;
+  cursor: pointer;
+`;
+
+const DefaultTabBtn = styled.div`
+  color: ${({ theme }) => theme.highlight};
+  background-color: ${({ theme }) => theme.major};
+  font-size: ${({ theme }) => theme.fontSize.large};
+  border: ${({ theme }) => `1px solid ${theme.highlight}`};
+  border-bottom: none;
+  border-radius: 10px 10px 0 0;
+  padding: 8px 16px;
+  cursor: pointer;
+`;
+
 export const S = {
   ProfileTabContainer,
   TabBtnContainer,
+  ActiveTabBtn,
+  DefaultTabBtn,
 };
