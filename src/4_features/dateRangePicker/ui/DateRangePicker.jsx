789952dbcc970bc5d3cb63591ee378afc55d3fce
mangkong1 <xogud5053@hanmail.net>
2024-07-16 00:47:15 +0900

    Refactor: 날짜 조회에 따른 필터링 함수 구현

--- src/4_features/dateRangePicker/ui/DateRangePicker.jsx
+++ src/4_features/dateRangePicker/ui/DateRangePicker.jsx
@@ -6,11 +6,42 @@ import { Icon, DefaultBtn } from "@shared/ui";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
+//예시 데이터
+const posts = [
+  { id: 1, title: "게시물 1", date: new Date(2024, 6, 14) },
+  { id: 2, title: "게시물 2", date: new Date(2024, 6, 15) },
+  { id: 3, title: "게시물 3", date: new Date(2024, 6, 16) },
+];
+
 export const DateRangePicker = () => {
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
   const [startDateSelected, setStartDateSelected] = useState(false);
   const [endDateSelected, setEndDateSelected] = useState(false);
+
+  const [filteredPosts, setFilteredPosts] = useState([]);
+
+  useEffect(() => {
+    if (!startDate && !endDate) {
+      setFilteredPosts(posts);
+    }
+  }, []);
+
+  const handleSearch = () => {
+    if (!startDate || !endDate) {
+      alert("시작 날짜와 종료 날짜를 모두 선택해주세요.");
+      return;
+    }
+    //예시 데이터 가공
+    const filtered = posts.filter((e) => {
+      const postDate = e.date;
+      const formatPostDate = postDate.toLocaleDateString("ko-KR");
+
+      return postDate >= startDate && postDate <= endDate;
+    });
+
+    setFilteredPosts(filtered);
+  };
 
   const formatDate = (date) => date.toLocaleDateString("ko-KR");
 
@@ -61,9 +92,18 @@ export const DateRangePicker = () => {
           }
         />
         <S.BtnContainer>
-          <DefaultBtn text="조회" />
+          <DefaultBtn text="조회" onClick={handleSearch} />
         </S.BtnContainer>
       </S.DatePickerRangeContainer>
+
+      <div>
+        {filteredPosts.map((post) => (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.date.toLocaleDateString("ko-KR")}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
