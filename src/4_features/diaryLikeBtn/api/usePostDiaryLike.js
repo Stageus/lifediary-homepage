6cc245fa621d5b2af1c76b77dfcb0bc91266a01c
junik <wkdwnsdlr37@gmail.com>
2024-07-15 10:59:18 +0900

    Feat: usePostDiaryLike 작성

--- src/4_features/diaryLikeBtn/api/usePostDiaryLike.js
+++ src/4_features/diaryLikeBtn/api/usePostDiaryLike.js
@@ -1,6 +1,46 @@
+import { useState, useEffect } from "react";
 import { useFetch, useCookie } from "@shared/hook";
 
-export const usePostDiaryLike = () => {
+export const usePostDiaryLike = ( isLiked, likeCnt ) => {
+    const [ _, status, baseFetch ] = useFetch();
+    const { handleGetCookie } = useCookie();
+    const [ isLike, setIsLike ] = useState( isLiked );
+    const [ likeCount, setLikeCount ] = useState( likeCnt );
+
+    const changeLikeInfo = () => {
+        if ( isLike ) setLikeCount( likeCount - 1);
+        if ( !isLike ) setLikeCount( likeCount + 1);
+        setIsLike( !isLike );
+    }
+
+    const postDiaryLike = ( diaryIdx ) => {
+        // 임시데이터
+        changeLikeInfo();
+
+        // 임시주석
+        // baseFetch( `diary/${diaryIdx}/like`, {method: "POST"}, handleGetCookie() );
+    };
+
+    useEffect(()=>{
+
+        if(status === 200){
+            changeData();
+            return ;
+        }
+        if(status === 400){
+            return console.log("유효성 검사 실패일경우");
+        }
+        if(status === 401){
+            return console.log("토큰이 잘못된경우 (없는경우)");
+        }
+        if(status === 404){
+            return console.log("대상 diaryIdx가 없는경우");
+        }
+        if(status === 500){
+            return console.log("서버에러");
+        }
+
+    },[status])
     
-    return [];
+    return [ isLike, likeCount, postDiaryLike ];
 }
