8af67e764464ab163da01a5c85d80f85bbb382b7
junik <wkdwnsdlr37@gmail.com>
2024-07-16 21:49:06 +0900

    Refactor: useModel 로직 적용

--- src/4_features/subscribeBtn/ui/SubscribeBtn.jsx
+++ src/4_features/subscribeBtn/ui/SubscribeBtn.jsx
@@ -1,27 +1,29 @@
+// Slice
 import { S } from "./style";
-import { useModel } from "../model/useModel";
+import { usePostSubscribe } from "../api/usePostSubscribe";
+// Layer
 import { DefaultBtn } from "@shared/ui";
 
-export const SubscribeBtn = (props)=>{
+export const SubscribeBtn = ( props )=>{
     const { isSubscribed, accountIdx } = props;
-    const {subscribe, onClickSubscribe} = useModel(isSubscribed, accountIdx);
+    const [ subscribe, postSubscribe ] = usePostSubscribe(isSubscribed);
 
     return(
         <>
             <S.SubscribeBtn>
-            {subscribe 
-            ? <DefaultBtn
-            text="구독중"
-            type="select"
-            size="medium"
-            onClick={onClickSubscribe}
-            />
-            :<DefaultBtn
-            text="구독"
-            size="medium"
-            onClick={onClickSubscribe}
-            />
-            }
+                {subscribe 
+                    ? <DefaultBtn
+                    text="구독중"
+                    type="select"
+                    size="medium"
+                    onClick={ () => postSubscribe(accountIdx) }
+                    />
+                    :<DefaultBtn
+                    text="구독"
+                    size="medium"
+                    onClick={() => postSubscribe(accountIdx)}
+                    />
+                }
             </S.SubscribeBtn>
         </>
     );
