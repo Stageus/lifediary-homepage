f15acd698f21fbef9017e4de36f7eafd0a191ba9
Jangmy <108041161+JangIkIk@users.noreply.github.com>
2024-07-16 21:31:57 +0900

    Complain page (#76)
    * Docs: complainContent 생성
    
    * Refactor: ComplainPage widgets 적용
    
    * Design: 레이아웃 임시 라인삭제
    
    * Design: 기본본UI 생성
    
    * Docs: useGetComplainList 생성
    
    * Refactor: 조건 추가
    
    * Docs: createTestData 함수 생성
    
    * Docs: 파일 및 함수명 변경
    
    * Docs: createTestData 생성
    
    * Refactor: state수정 및 state병합
    
    * Refactor: 데이터 바인딩
    
    * Feat: divideToArray 함수생성
    
    * Refactor: api page전환 함수 추가
    
    * Refactor: createTestData 조건 수정
    
    * Refactor: 의존성 배열추가
    
    * Feat: onClick 이벤트 적용
    
    * Feat: useGetComplainInfo Put 요청 추가:
    
    * Docs: useGetComplainInfo 이름변경 -> useComplainInfo
    
    * Feat: ui컴포넌트 put요청 적용
    
    * Refactor: complainAlarm loaction 추가
    
    * None
    
    * Refactor: 기능관련 주석 추가
    
    * None
    
    * None
    
    * Bug
    
    * Refactor: ComplainAlarm navigate 변경
    
    * None
    
    * None
    
    * Refactor: 코드 분할
    
    * Feat: Router back 설정
    
    * Design: Layout 크기 변경
    
    * Design: Layout 크기변경으로 이한 고정크기변경
    
    * Design: Complain Css 크기변경
    
    * Refactor: Css 속성 추가 및 삭제
    
    * Refactor: TypeError 수정
    
    * None
    
    * None
    
    * Docs: ComplainContent 폴더 삭제
    
    * Refactor: navigate 경로 변경
    
    * Docs: useModel 삭제
    
    * Docs: lib 함수 간격 및 수정
    
    * None
    
    * Refactor: useGetComplainList 로직 변경
    
    * Refactor: pagaBtn 조건 추가
    
    * Refactor: useModel 삭제
    
    * Refactor: usePutComplain 조건수정
    
    * Refactor: 로직 적용

--- src/2_pages/complain/api/useGetComplainList.js
+++ src/2_pages/complain/api/useGetComplainList.js
@@ -1,63 +1,57 @@
+// Npm
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import { useFetch, useCookie } from "@shared/hook";
+// Slice
 import { mapper } from "../lib/mapper";
+// Layer
+import { useFetch, useCookie } from "@shared/hook";
 // 임시데이터
 import { createTestData } from "../service/createTestData";
 
-export const useGetComplainList = ()=>{
-    const [complainList, setComplainList] = useState({});
-    const [fetchData, status, baseFetch] = useFetch();
+export const useGetComplainList = () => {
+    
+    const [ complainList, setComplainList ] = useState( null );
+    const [ fetchData, status, baseFetch ] = useFetch();
     const { handleGetCookie } = useCookie();
-    const [searchParams, setSearchParams] = useSearchParams();
-    const currentPage = ()=> searchParams.get("page");
+    const [ searchParams, setSearchParams ] = useSearchParams();
+    const currentPage = ()=> searchParams.get( "page" );
 
-    const changePage = (pageNumber)=>{
-        setSearchParams({page: pageNumber})
-    }
+    const changePage = ( pageNumber )=> {
+        setSearchParams( {page: pageNumber} )
+    };
 
-    const getComplainList = ()=>{
-        baseFetch(`report?page=${searchParams.size}`,{},handleGetCookie());
-    }
+    const getComplainList = ()=>{ 
 
-    useEffect(()=>{
-        // 임시데이터
-        if(searchParams.size === 0){
-            setComplainList({...mapper(createTestData(1))});    
-        }else{
-            setComplainList({...mapper(createTestData(searchParams.get("page")))});
+        if ( !searchParams.size ) {
+            setSearchParams( { page: 1 } );
+            // 임시 데이터(삭제예정)
+            setComplainList({...mapper( createTestData( 1 ) )});
+            // baseFetch(`report?page=${1}`,{},handleGetCookie());
+            return;
         }
+
+        // 임시 데이터(삭제예정)
+        setComplainList({...mapper( createTestData( searchParams.get( "page" ) ) )}); 
         // 임시주석
-        // getComplainList();
-    },[searchParams])
+        // baseFetch(`report?page=${searchParams.get( "page" )}`,{},handleGetCookie());
+    };
 
-    useEffect(()=>{
-        if(status === 200){
-            setComplainList(mapper(fetchData));
-        }
+    useEffect(() => {
 
-        if(status === 400){
-            return console.log("유효성 검사 실패일 경우");
-        }
+        getComplainList();
 
-        if(status === 401){
-            return console.log("토큰이 없는경우, 잘못된경우");
-        }
+    },[searchParams]);
 
-        if(status === 403){
-            return console.log("관리자가 아닐경우");
-        }
+    useEffect(() => {
 
-        // 이게 필요한가?
-        if(status === 404){
-            return console.log("더이상 작성된 신고가 없을경우");
-        }
+        if ( status === 200 ) return setComplainList(mapper(fetchData));
+        if ( status === 400 ) return console.log("유효성 검사 실패일 경우");
+        if ( status === 401 ) return console.log("토큰이 없는경우, 잘못된경우");
+        if ( status === 403 ) return console.log("관리자가 아닐경우");
+        if ( status === 404 ) return console.log("더이상 작성된 신고가 없을경우");
+        if ( status === 500 ) return console.log("서버 에러");
 
-        if(status === 500){
-            return console.log("서버 에러");
-        }
-
-    },[status])
+    },[fetchData]);
     
     return [complainList, currentPage, changePage];
 }
