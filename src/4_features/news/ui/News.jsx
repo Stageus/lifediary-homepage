dcb2b1f75858eaba4419233e63431d8d9fb6592f
junik <wkdwnsdlr37@gmail.com>
2024-07-17 12:03:48 +0900

    Docs: news 폴더 생성

---
+++ src/4_features/news/ui/News.jsx
@@ -0,0 +1,14 @@
+import { S } from "./style";
+import { DefaultBtn } from "@shared/ui"; 
+
+export const News = () => {
+    return(
+        <>
+            <div>
+                <DefaultBtn
+                text= "알림"
+                />
+            </div>
+        </>
+    );
+}
