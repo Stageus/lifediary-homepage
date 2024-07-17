0a22786d24372d5fbde72f0e89692eef428953cf
mangkong1 <xogud5053@hanmail.net>
2024-07-18 02:16:39 +0900

    Docs: widget레이어 코드 pages레이어 코드로 이동

--- src/2_pages/search/ui/Search.jsx
+++ src/2_pages/search/ui/Search.jsx
@@ -1,9 +1,42 @@
-import { SearchContent } from "@widgets/searchContent/ui/SearchContent";
+import React from "react";
+
+import { S } from "./style";
+import { useGetSearchContent } from "../api/useGetSearchContent";
+import { navigatePage } from "../lib/navigatePage";
 
 export const Search = () => {
+  const searchData = useGetSearchContent();
+  const [navigateMyProfile, navigateDiary] = navigatePage();
+
+  if (!searchData || searchData.length === 0) {
+    return <div>로딩 중...</div>;
+  }
+
   return (
     <>
-      <SearchContent />
+      <S.SearchContents>
+        {searchData.map((item) => (
+          <S.SearchContentContainer key={item.idx}>
+            <S.ContentInfoContainer>
+              <S.UserInfoContainer onClick={navigateMyProfile}>
+                <S.ProfileImg src={item.profileImg} />
+                <p>{item.nickname}</p>
+              </S.UserInfoContainer>
+              <S.DiaryInfoContainer>
+                <p>{item.createdAt}</p>
+                <p>{`좋아요 ${item.likeCnt}개`}</p>
+                <p>{item.tags.map((tag) => "#" + tag)}</p>
+              </S.DiaryInfoContainer>
+            </S.ContentInfoContainer>
+            <S.ThumbnailImgContainer onClick={navigateDiary}>
+              <S.ThumbnailImg src={item.thumbnailImg} />
+            </S.ThumbnailImgContainer>
+            <S.TextContentContainer onClick={navigateDiary}>
+              <p>{item.textContent}</p>
+            </S.TextContentContainer>
+          </S.SearchContentContainer>
+        ))}
+      </S.SearchContents>
     </>
   );
 };
