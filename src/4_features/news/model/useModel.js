d6a64361da6d95d98953517d286e1512ca884630
junik <wkdwnsdlr37@gmail.com>
2024-07-17 14:07:58 +0900

    Refactor: useModel 이벤트함수 추가

--- src/4_features/news/model/useModel.js
+++ src/4_features/news/model/useModel.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import { useGetNewNotice } from "../api/useGetNewNotice";
 
-export const useModel = () => {
-    const [isNew] = useGetNewNotice();
+/*
+    "newComment": 내일기에 댓글이 달렸을 경우
+    "newDiary": 구독자가 일기를 작성했을 경우
+    "deletedMyDiary": 내일기가 신고받아 삭제되었을 경우
+    "deletedDiary": 신고한일기가 삭제되었을 경우
+    "recoveredDiary": 내일기가 복구되었을 경우
+*/
 
-    return { isNew };
+export const useModel = () => {
+    const [ isNew ] = useGetNewNotice();
+    const [ isOpenModal, setIsOpenModal ] = useState( false );
+    const onClickOpen = () => setIsOpenModal( !isOpenModal );
+
+    return { isNew, isOpenModal, onClickOpen};
 }
