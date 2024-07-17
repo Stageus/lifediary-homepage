28c8084101321f8c163f1c44ce958315419d4398
Jangmy <108041161+JangIkIk@users.noreply.github.com>
2024-07-17 14:19:53 +0900

    News api hook 기본정의 (#82)
    * Refactor: header NewsBtn 적용
    
    * Docs: news 폴더 생성
    
    * Design: ComplainModal css 변경
    
    * Design: UI 적용
    
    * Design: Scroll 적용
    
    * reset
    
    * Docs: 삭제된 파일 복구
    
    * Docs: useGetNewNotice 생성
    
    * Docs: useModel 생성
    
    * Refactor: useModel 이벤트함수 추가
    
    * Refactor: useModel 적용 및 icon 추가
    
    * Docs: useDeleteNotice 생성
    
    * Refactor: useModel api hook 추가
    
    * Refactor: deleteNotice 함수 click 이벤트적용
    
    * Docs: useGetNoticeList 생성

--- src/4_features/news/ui/style.js
+++ src/4_features/news/ui/style.js
@@ -4,6 +4,18 @@ const News = styled.div`
     position: relative;
 `;
 
+const NewsAlarm = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(50%, -50%);
+    background-color: ${({ theme }) => theme.highlight};
+    display: flex;
+    justify-content: center;
+    border-radius: 50%;
+    padding: 1px;
+`;
+
 const NewsBtnWrap = styled.div`
     box-shadow: 1px 1px 3px gray;
     border-radius: 4px;
@@ -84,6 +96,7 @@ const DeleteBtnWrap = styled.div`
 
 export const S = {
     News,
+    NewsAlarm,
     NewsBtnWrap,
     NewsModal,
     NewsItem,
