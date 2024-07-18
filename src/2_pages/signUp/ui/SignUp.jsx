959fd9d4013ed32d6d1a06f5295d07f28b146590
mangkong1 <xogud5053@hanmail.net>
2024-07-18 17:30:45 +0900

    Docs: widgets레이어 코드 pages레이어와 통합

--- src/2_pages/signUp/ui/SignUp.jsx
+++ src/2_pages/signUp/ui/SignUp.jsx
@@ -1,13 +1,49 @@
-import { SignUpInfo } from "@features/signUpInfo/ui/SignUpInfo";
+import { useNavigate, useLocation } from "react-router-dom";
+
 import { S } from "./style";
+import { DefaultBtn, TextInput } from "@shared/ui";
+import { usePostSignUpInfo } from "../api/usePostSignUpInfo";
+import { useProfileForm } from "../lib/useProfileForm";
 
 export const SignUp = () => {
+  const { profileImg, nickname, inputType, imageInput, profileImgMessage, btnMessage, btnType, handleChangeImgBase, handleChangeProfileImg, handleCheckInputValue } = useProfileForm();
+  const navigate = useNavigate();
+  const [postSignUpInfo] = usePostSignUpInfo();
+
+  const location = useLocation();
+  // const oauthGoogleId = location.state.userData.googleOauthId;
+
+  const handleUploadInfo = () => {
+    postSignUpInfo(profileImg, nickname, oauthGoogleId);
+    navigate("/");
+  };
+
   return (
     <>
       <S.PageContainer>
         <S.ProfileContainer>
-          <S.Logo />
-          <SignUpInfo />
+          <S.Logo onClick={() => navigate("/")} />
+          <S.SignUpInfoContainer>
+            <S.SignUpInfoProfileImgContainer>
+              {/* ref속성으로 imageInput을 설정해 입력요소의 참조를 imageInput.current에 저장 */}
+              <input type="file" hidden ref={imageInput} onChange={handleChangeImgBase} />
+              {profileImg && <S.SignUPInfoProfileImg src={profileImg} onClick={handleChangeProfileImg} />}
+              <S.SignUpInfoMessage>{profileImgMessage}</S.SignUpInfoMessage>
+            </S.SignUpInfoProfileImgContainer>
+            <S.SignUpInfoNicknameContainer>
+              <S.SignUpInfoNicknameLabel>닉네임</S.SignUpInfoNicknameLabel>
+              <S.SignUpInfoNicknameInputContainer>
+                <TextInput type={inputType} placeholder="닉네임을 입력해주세요(최대 20자)" onBlur={handleCheckInputValue} onChange={handleCheckInputValue}>
+                  {nickname}
+                </TextInput>
+                <S.SignUpInfoMessage>{btnMessage}</S.SignUpInfoMessage>
+              </S.SignUpInfoNicknameInputContainer>
+            </S.SignUpInfoNicknameContainer>
+            <S.SignUpInfoBtnContainer>
+              <DefaultBtn type={btnType} text="작성" onClick={handleUploadInfo} />
+              <DefaultBtn text="취소" onClick={() => navigate("/Login")} />
+            </S.SignUpInfoBtnContainer>
+          </S.SignUpInfoContainer>
         </S.ProfileContainer>
       </S.PageContainer>
     </>
