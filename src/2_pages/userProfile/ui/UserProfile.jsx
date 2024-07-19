32db3e0ba92a3b7b22ddad0befa57f70c2f37faf
mangkong1 <xogud5053@hanmail.net>
2024-07-20 02:37:24 +0900

    Feat: UserPage ui 생성, userDiaryList 생성

--- src/2_pages/userProfile/ui/UserProfile.jsx
+++ src/2_pages/userProfile/ui/UserProfile.jsx
@@ -2,22 +2,23 @@ import { useState } from "react";
 
 import { S } from "./style.js";
 import { ProfileInfo } from "@features/profileInfo";
-import { ProfileTab } from "@features/profileTab";
-import { DateRangePicker } from "@features/dateRangePicker";
+import { UserDiaryList } from "@features/userDiaryList";
 
 export const UserProfile = () => {
   const [selectedTab, setSelectedTab] = useState("myDiaryTab");
+
+  const handleTabBtnClick = (e) => {
+    setSelectedTab(e);
+  };
 
   return (
     <>
       <S.MyProfileContentContainer>
         <ProfileInfo />
-        <ProfileTab onTabSelect={setSelectedTab} />
-        {selectedTab === "myDiaryTab" ? (
-          <>
-            <DateRangePicker />
-          </>
-        ) : null}
+        <S.ProfileTabContainer>
+          <S.TabBtnContainer onClick={() => handleTabBtnClick("myDiaryTab")}>{selectedTab === "myDiaryTab" ? <S.ActiveTabBtn>유저 일기</S.ActiveTabBtn> : <S.DefaultTabBtn>유저 일기</S.DefaultTabBtn>}</S.TabBtnContainer>
+        </S.ProfileTabContainer>
+        {selectedTab === "myDiaryTab" ? <UserDiaryList /> : null}
       </S.MyProfileContentContainer>
     </>
   );
