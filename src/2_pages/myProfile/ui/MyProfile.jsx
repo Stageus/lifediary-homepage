350d589e0c2269fd28203cfd01e83dfbba7a3d0d
mangkong1 <xogud5053@hanmail.net>
2024-07-20 01:48:51 +0900

    Refactor: profileTab코드 myProfile에 병합

--- src/2_pages/myProfile/ui/MyProfile.jsx
+++ src/2_pages/myProfile/ui/MyProfile.jsx
@@ -2,17 +2,23 @@ import { useState } from "react";
 
 import { S } from "./style.js";
 import { ProfileInfo } from "@features/profileInfo";
-import { ProfileTab } from "@features/profileTab";
 import { MyDiaryList } from "@features/myDiaryList";
 
 export const MyProfile = () => {
   const [selectedTab, setSelectedTab] = useState("myDiaryTab");
 
+  const handleTabBtnClick = (e) => {
+    setSelectedTab(e);
+  };
+
   return (
     <>
       <S.MyProfileContentContainer>
         <ProfileInfo />
-        <ProfileTab onTabSelect={setSelectedTab} />
+        <S.ProfileTabContainer>
+          <S.TabBtnContainer onClick={() => handleTabBtnClick("myDiaryTab")}>{selectedTab === "myDiaryTab" ? <S.ActiveTabBtn>내 일기</S.ActiveTabBtn> : <S.DefaultTabBtn>내 일기</S.DefaultTabBtn>}</S.TabBtnContainer>
+          <S.TabBtnContainer onClick={() => handleTabBtnClick("likeDiaryTab")}>{selectedTab === "likeDiaryTab" ? <S.ActiveTabBtn>좋아요 표시한 일기</S.ActiveTabBtn> : <S.DefaultTabBtn>좋아요 표시한 일기</S.DefaultTabBtn>}</S.TabBtnContainer>
+        </S.ProfileTabContainer>
         {selectedTab === "myDiaryTab" ? <MyDiaryList /> : null}
       </S.MyProfileContentContainer>
     </>
