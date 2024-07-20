2618fb3ac51cd5a113e61f3d71c7f9c6b2be5f70
mangkong1 <xogud5053@hanmail.net>
2024-07-20 12:57:09 +0900

    Feat: UserProfileInfo생성 및 구독버튼 삽입

--- src/2_pages/userProfile/ui/UserProfile.jsx
+++ src/2_pages/userProfile/ui/UserProfile.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import { S } from "./style.js";
-import { ProfileInfo } from "@features/profileInfo";
+import { UserProfileInfo } from "@features/userProfileInfo";
 import { UserDiaryList } from "@features/userDiaryList";
 
 export const UserProfile = () => {
@@ -14,7 +14,7 @@ export const UserProfile = () => {
   return (
     <>
       <S.MyProfileContentContainer>
-        <ProfileInfo />
+        <UserProfileInfo />
         <S.ProfileTabContainer>
           <S.TabBtnContainer onClick={() => handleTabBtnClick("myDiaryTab")}>{selectedTab === "myDiaryTab" ? <S.ActiveTabBtn>유저 일기</S.ActiveTabBtn> : <S.DefaultTabBtn>유저 일기</S.DefaultTabBtn>}</S.TabBtnContainer>
         </S.ProfileTabContainer>
