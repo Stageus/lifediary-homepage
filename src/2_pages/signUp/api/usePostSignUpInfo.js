92ec37d64fd352e28940395e3c57d61713de3721
mangkong1 <xogud5053@hanmail.net>
2024-07-23 02:04:46 +0900

    Refactor: 로직 파일 분리

--- src/2_pages/signUp/api/usePostSignUpInfo.js
+++ src/2_pages/signUp/api/usePostSignUpInfo.js
@@ -1,31 +1,38 @@
-import { useEffect } from "react";
-
+import { useState, useEffect } from "react";
 import { useFetch, useCookie } from "@shared/hook";
 
 export const usePostSignUpInfo = () => {
-  const [signUpData, errorStatus, baseFetch] = useFetch();
+  const [signUpData, status, baseFetch] = useFetch();
   const { handleGetCookie } = useCookie();
 
   const postSignUpInfo = (profileImg, nickname, oauthGoogleId) => {
+    const formData = new FormData();
+    formData.append("profileImg", profileImg);
+    formData.append("nickname", nickname);
+    formData.append("oauthGoogleId", oauthGoogleId);
+
     baseFetch(
       "account",
       {
         method: "POST",
-        data: { profileImg, nickname, oauthGoogleId },
+        body: formData,
       },
       handleGetCookie()
     );
   };
 
   useEffect(() => {
-    if (errorStatus === 400) {
+    postSignUpInfo();
+  }, [signUpData]);
+
+  useEffect(() => {
+    if (status === 400) {
       return console.log("유효성 검사 실패");
     }
-
-    if (errorStatus === 500) {
+    if (status === 500) {
       return console.log("서버 에러");
     }
-  }, [errorStatus]);
+  }, [status]);
 
-  return [postSignUpInfo, signUpData, errorStatus, baseFetch];
+  return [signUpData, status, baseFetch];
 };
--- src/2_pages/signUp/ui/SignUp.jsx
+++ src/2_pages/signUp/ui/SignUp.jsx
@@ -1,21 +1,33 @@
+import { useState, useEffect, useRef } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
 import { S } from "./style";
-import { DefaultBtn, TextInput } from "@shared/ui";
+import { useChangeImgBase } from "../lib/useChangeImgBase";
+import { useCheckInputValue } from "../lib/useCheckInputValue";
 import { usePostSignUpInfo } from "../api/usePostSignUpInfo";
-import { useProfileForm } from "../lib/useProfileForm";
+
+import { DefaultBtn, TextInput } from "@shared/ui";
 
 export const SignUp = () => {
-  const { profileImg, nickname, inputType, imageInput, profileImgMessage, btnMessage, btnType, handleChangeImgBase, handleChangeProfileImg, handleCheckInputValue } = useProfileForm();
   const navigate = useNavigate();
-  const [postSignUpInfo] = usePostSignUpInfo();
+  const [btnType, setBtnType] = useState("disabled");
+  const imageInput = useRef(null);
+  const [handleChangeImgBase, profileImg, profileImgMessage, isProfileImgValid] = useChangeImgBase();
+  const [handleCheckInputValue, nickname, inputType, btnMessage, isNicknameValid] = useCheckInputValue();
+  const [signUpData, status, baseFetch] = usePostSignUpInfo();
+
+  useEffect(() => {
+    if (isProfileImgValid && isNicknameValid) {
+      setBtnType("");
+    }
+  }, [isProfileImgValid, isNicknameValid]);
 
   const location = useLocation();
   // const oauthGoogleId = location.state.userData.googleOauthId;
+  const oauthGoogleId = "2";
 
   const handleUploadInfo = () => {
-    postSignUpInfo(profileImg, nickname, oauthGoogleId);
-    navigate("/");
+    baseFetch(profileImg, nickname, oauthGoogleId);
   };
 
   return (
@@ -27,20 +39,20 @@ export const SignUp = () => {
             <S.SignUpInfoProfileImgContainer>
               {/* ref속성으로 imageInput을 설정해 입력요소의 참조를 imageInput.current에 저장 */}
               <input type="file" hidden ref={imageInput} onChange={handleChangeImgBase} />
-              {profileImg && <S.SignUPInfoProfileImg src={profileImg} onClick={handleChangeProfileImg} />}
+              {profileImg && <S.SignUPInfoProfileImg src={profileImg} onClick={() => imageInput.current.click()} />}
               <S.SignUpInfoMessage>{profileImgMessage}</S.SignUpInfoMessage>
             </S.SignUpInfoProfileImgContainer>
             <S.SignUpInfoNicknameContainer>
               <S.SignUpInfoNicknameLabel>닉네임</S.SignUpInfoNicknameLabel>
               <S.SignUpInfoNicknameInputContainer>
-                <TextInput type={inputType} placeholder="닉네임을 입력해주세요(최대 20자)" onBlur={handleCheckInputValue} onChange={handleCheckInputValue}>
+                <TextInput type={inputType} placeholder="닉네임을 입력해주세요(최대 20자)" onBlur={handleCheckInputValue}>
                   {nickname}
                 </TextInput>
                 <S.SignUpInfoMessage>{btnMessage}</S.SignUpInfoMessage>
               </S.SignUpInfoNicknameInputContainer>
             </S.SignUpInfoNicknameContainer>
             <S.SignUpInfoBtnContainer>
-              <DefaultBtn type={btnType} text="작성" onClick={handleUploadInfo} />
+              <DefaultBtn type={btnType} text="작성" onClick={() => handleUploadInfo()} />
               <DefaultBtn text="취소" onClick={() => navigate("/Login")} />
             </S.SignUpInfoBtnContainer>
           </S.SignUpInfoContainer>
