dc8c2ddbfd607c3f302ba73b91da2f07248afddd
mangkong1 <xogud5053@hanmail.net>
2024-07-22 20:43:40 +0900

    Refactor: createTag제거 및 tagInput과 연결

--- src/2_pages/diaryCreate/ui/DiaryCreate.jsx
+++ src/2_pages/diaryCreate/ui/DiaryCreate.jsx
@@ -3,9 +3,8 @@ import { useNavigate } from "react-router-dom";
 
 import { S } from "./style";
 import { usePostDiaryInfo } from "../api/usePostDiaryInfo";
-import { DefaultBtn } from "@shared/ui";
+import { DefaultBtn, TagInput } from "@shared/ui";
 import { CreateImg } from "@features/createImg";
-import { CreateTag } from "@features/createTag";
 import { CreateGrass } from "@features/createGrass";
 import { CreatePublic } from "@features/createPublic";
 
@@ -18,14 +17,6 @@ export const DiaryCreate = () => {
   const [diaryData, status, baseFetch] = usePostDiaryInfo();
   const navigate = useNavigate();
 
-  const handleSubmit = () => {
-    if (color === "") {
-      alert("색상을 선택해주세요.");
-    } else {
-      baseFetch(imgContents, textContent, tags, isPublic, color);
-    }
-  };
-
   const checkTextLength = (e) => {
     const inputText = e.target.value;
     if (inputText.length < 500) {
@@ -33,6 +24,14 @@ export const DiaryCreate = () => {
     } else {
       alert("입력 가능한 최대 글자수는 500자입니다.");
       setTextContent(inputText.substr(0, 500)); // substr() 메서드는 문자열에서 특정 위치에서 시작하여 특정 문자 수만큼의 문자들을 반환
+    }
+  };
+
+  const handleSubmit = () => {
+    if (color === "") {
+      alert("잔디 색상을 선택해주세요.");
+    } else {
+      baseFetch(imgContents, textContent, tags, isPublic, color);
     }
   };
 
@@ -44,12 +43,15 @@ export const DiaryCreate = () => {
           <S.TextContent onChange={checkTextLength} maxLength="500" />
         </S.ContentContainer>
         <CreateImg onImgContentsChange={setImgContents} />
-        <CreateTag onTagsChange={setTags} />
-        <CreateGrass onColorSelected={() => setColor(color)} />
+        <S.ContentContainer>
+          <S.ContentNameContainer>태그</S.ContentNameContainer>
+          <TagInput placeholder="입력 후 엔터를 누르면 태그가 자동으로 입력됩니다 (최대 3개)" onTagsChange={setTags} />
+        </S.ContentContainer>
+        <CreateGrass onColorSelected={setColor} />
         <CreatePublic onIsPublicChange={setIsPublic} />
         <S.BtnContainer>
           <div>
-            <DefaultBtn text="작성" type={color !== "" ? "select" : "disabled"} onClick={handleSubmit} />
+            <DefaultBtn text="작성" type={color === "" ? "disabled" : ""} onClick={handleSubmit} />
           </div>
           <div>
             <DefaultBtn text="취소" onClick={() => navigate(-1)} />
