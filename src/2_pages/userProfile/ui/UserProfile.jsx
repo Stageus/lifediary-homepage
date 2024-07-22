619019f1ab39c52c0e9fc288ae081b8b394e2960
buzzkim <97722152+mangkong1@users.noreply.github.com>
2024-07-23 02:14:52 +0900

    UserDiaryList 생성 (#92)
    * Feat: UserProfile ui 생성
    
    * Feat: UserPage ui 생성, userDiaryList 생성
    
    * Feat: UserProfileInfo생성 및 구독버튼 삽입

--- src/2_pages/userProfile/ui/UserProfile.jsx
+++ src/2_pages/userProfile/ui/UserProfile.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
+import { S } from "./style.js";
+import { UserProfileInfo } from "@features/userProfileInfo";
+import { UserDiaryList } from "@features/userDiaryList";
+
 export const UserProfile = () => {
-    return(
-        <>
-            <div>유저 페이지</div>
-        </>
-    );
-}
+  const [selectedTab, setSelectedTab] = useState("myDiaryTab");
+
+  const handleTabBtnClick = (e) => {
+    setSelectedTab(e);
+  };
+
+  return (
+    <>
+      <S.MyProfileContentContainer>
+        <UserProfileInfo />
+        <S.ProfileTabContainer>
+          <S.TabBtnContainer onClick={() => handleTabBtnClick("myDiaryTab")}>{selectedTab === "myDiaryTab" ? <S.ActiveTabBtn>유저 일기</S.ActiveTabBtn> : <S.DefaultTabBtn>유저 일기</S.DefaultTabBtn>}</S.TabBtnContainer>
+        </S.ProfileTabContainer>
+        {selectedTab === "myDiaryTab" ? <UserDiaryList /> : null}
+      </S.MyProfileContentContainer>
+    </>
+  );
+};
