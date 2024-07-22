7404bd00cedc272464552bb76657392f37f1cf08
mangkong1 <xogud5053@hanmail.net>
2024-07-22 21:27:41 +0900

    Refactor: 글자수제한 이벤트 분리

--- src/2_pages/diaryCreate/ui/DiaryCreate.jsx
+++ src/2_pages/diaryCreate/ui/DiaryCreate.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 
 import { S } from "./style";
 import { usePostDiaryInfo } from "../api/usePostDiaryInfo";
+import { useCheckTextLength } from "../lib/useCheckTextLength";
+
 import { DefaultBtn, TagInput } from "@shared/ui";
 import { CreateImg } from "@features/createImg";
 import { CreateGrass } from "@features/createGrass";
@@ -10,22 +12,12 @@ import { CreatePublic } from "@features/createPublic";
 
 export const DiaryCreate = () => {
   const [imgContents, setImgContents] = useState([]);
-  const [textContent, setTextContent] = useState("");
   const [tags, setTags] = useState([]);
   const [isPublic, setIsPublic] = useState(false);
   const [color, setColor] = useState("");
   const [diaryData, status, baseFetch] = usePostDiaryInfo();
   const navigate = useNavigate();
-
-  const checkTextLength = (e) => {
-    const inputText = e.target.value;
-    if (inputText.length < 500) {
-      setTextContent(inputText);
-    } else {
-      alert("입력 가능한 최대 글자수는 500자입니다.");
-      setTextContent(inputText.substr(0, 500)); // substr() 메서드는 문자열에서 특정 위치에서 시작하여 특정 문자 수만큼의 문자들을 반환
-    }
-  };
+  const [checkTextLength, textContent, setTextContent] = useCheckTextLength(500);
 
   const handleSubmit = () => {
     if (color === "") {
@@ -40,12 +32,12 @@ export const DiaryCreate = () => {
       <S.DiaryCreateContainer>
         <S.ContentContainer>
           <S.ContentNameContainer>내용</S.ContentNameContainer>
-          <S.TextContent onChange={checkTextLength} maxLength="500" />
+          <S.TextContent onChange={checkTextLength} />
         </S.ContentContainer>
         <CreateImg onImgContentsChange={setImgContents} />
         <S.ContentContainer>
           <S.ContentNameContainer>태그</S.ContentNameContainer>
-          <TagInput placeholder="입력 후 엔터를 누르면 태그가 자동으로 입력됩니다 (최대 3개)" onTagsChange={setTags} />
+          <TagInput placeholder="입력 후 엔터를 누르면 태그 자동 입력 (최대 3개)" onTagsChange={setTags} />
         </S.ContentContainer>
         <CreateGrass onColorSelected={setColor} />
         <CreatePublic onIsPublicChange={setIsPublic} />
