2106c24dcb6dfcbfdf0ba955a8d56937138aba57
mangkong1 <xogud5053@hanmail.net>
2024-07-23 19:08:19 +0900

    Refactor: api 변수명 수정

--- src/4_features/googleLogin/api/useGetRedirectUrl.js
+++ src/4_features/googleLogin/api/useGetRedirectUrl.js
@@ -3,25 +3,18 @@ import { useEffect } from "react";
 import { useFetch, useCookie } from "@shared/hook";
 
 export const useGetRedirectUrl = () => {
-  const [redirecUrltData, status, baseFetch] = useFetch();
+  const [fetchData, status, baseFetch] = useFetch();
   const { handleSetCookie } = useCookie();
 
-  const getRedirectUrl = () => {
+  useEffect(() => {
     baseFetch("login/oauth/google", {}, handleSetCookie());
-  };
+  }, [fetchData]);
 
   useEffect(() => {
-    getRedirectUrl();
-  }, []);
-
-  useEffect(() => {
-    if (status === 200) {
-      console.log("구글 로그인 페이지로 이동");
-    }
     if (status === 500) {
       console.log("서버 에러");
     }
-  }, [status]);
+  }, [fetchData]);
 
-  return [redirecUrltData, status, baseFetch];
+  return [fetchData, status, baseFetch];
 };
