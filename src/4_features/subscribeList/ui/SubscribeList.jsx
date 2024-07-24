dfb2abc705ee185302f0dbc36a6db665c4bf418a
Jangmy <108041161+JangIkIk@users.noreply.github.com>
2024-07-24 11:41:17 +0900

    구독리스트 수정 (#106)
    * None
    
    * Docs: SubscribeList 기본 구조생성
    
    * docs: createTestData 생성
    
    * Design: UI 생성
    
    * Docs: useGetSubscribeList 생성
    
    * Docs: useGetSubscribeList 생성
    
    * None
    
    * Refactor: TestData 적용
    
    * Pull
    
    * Refactor: useFetch 상태코드 변경
    
    * None
    
    * Refactor: ApiHook 예외처리 변경
    
    * Refactor: apiHook addPage함수추가
    
    * Feat: mapper 생성
    
    * Refactor: useGetSubScribeList 조건 수정
    
    * Refactor: createTestData 속성명 수정
    
    * Feat: Scroll 이벤트 적용
    
    * Feat: navigate 적용 및 css 추가
    
    * Refactor: api hook 예외처리 변경
    
    * Refactor: useScroll 적용

--- src/4_features/subscribeList/ui/SubscribeList.jsx
+++ src/4_features/subscribeList/ui/SubscribeList.jsx
@@ -1,44 +1,42 @@
-// Npm
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 // Slice
 import { S } from "./style";
+import { useRoute } from "../model/useRoute";
 import { useGetSubscribeList } from "../api/useGetSubscribeList";
 // Layer
 import { useScroll } from "@shared/hook";
 
 export const SubscribeList = () => {
 
-    const [ subscribeList, addPage ] = useGetSubscribeList();
-    const [ lastScroll, scrollRef, onScrollNext, onScrollReset ] = useScroll();
-    const navigate = useNavigate();
-    
-    const onClickRoute = ( accountidx ) => navigate(`/userpage/${accountidx}/mine`);
-
-    useEffect(() => {
-
-        if ( lastScroll ){
-            addPage();
-            // onScrollReset();
-        }
-
-    },[lastScroll])
+    const [ getSubscribeList, subscribeList, isLoading, errorMessage ] = useGetSubscribeList();
+    const [ rootRef, watchRef ] = useScroll( getSubscribeList );
+    const { onClickRoute } = useRoute();
 
     return(
         <>
         <S.SubscribeInfo>
             <S.SubscribeTitle>구독목록</S.SubscribeTitle>
 
-            <S.SubscribeList  ref={ scrollRef } onScroll={ onScrollNext }>
-            {subscribeList && subscribeList.map(( item )=>{
-              return(
-                  <S.SubscribeItem key={ item.toAccountIdx } onClick={ () => onClickRoute( item.toAccountIdx ) }>
-                      <img src={ item.profileImg } alt="#" />
-                      <span>{ item.nickname }</span>
-                  </S.SubscribeItem>
-              );
-            })}
+            <S.SubscribeList ref={ rootRef }>
+                {subscribeList && subscribeList.map(( item )=>{
+                return(
+                    <S.SubscribeItem key={ item.toAccountIdx } onClick={ () => onClickRoute( item.toAccountIdx ) }>
+                        <img src={ item.profileImg } alt="#" />
+                        <span>{ item.nickname }</span>
+                    </S.SubscribeItem>
+                );
+                })}
+
+                {subscribeList && subscribeList.length >= 20 
+                    && <div ref={ watchRef }></div>
+                }
+
+                { isLoading ? <S.Loading>로딩중...</S.Loading> : null}
+
+                { errorMessage ?? <div>{ errorMessage }</div>}
+
             </S.SubscribeList>
+
+            
           
         </S.SubscribeInfo>
         </>
