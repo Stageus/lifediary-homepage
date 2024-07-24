293784fb1f34c384f404c967f12c08a08de31413
junik <wkdwnsdlr37@gmail.com>
2024-07-24 12:36:51 +0900

    Refactor: complainAlarm 예외처리 변경

--- src/4_features/complainAlarm/api/useGetAlarm.js
+++ src/4_features/complainAlarm/api/useGetAlarm.js
@@ -5,27 +5,40 @@ import { useFetch, useCookie } from "@shared/hook";
 
 export const useGetAlarm = () => {
     
-    const [ alarm, setAlarm ] = useState( false );
-    const [ fetchData, status, baseFetch ] = useFetch();
+    const [ fetchData, baseFetch ] = useFetch();
     const { handleGetCookie } = useCookie();
+    const [ isNew, setIsNew ] = useState( false );
 
-    const getAlarm = ()=>{
-        baseFetch("report/new",{},handleGetCookie());
-    }
+    const getAlarm = ()=> baseFetch("report/new",{},handleGetCookie());
 
     useEffect(() => {
-        // 임시주석
         // getAlarm();
     },[])
 
     useEffect(() => {
+        if ( !fetchData ) return;
+        
+        switch ( fetchData.status ) {
+            case 200:
+                setIsNew(fetchData.data.isNew)
+                break;
 
-        if ( status === 200 ) return setAlarm(fetchData.isNew);
-        if ( status === 401 ) return console.log("토큰이 잘못되거나 없는경우");
-        if ( status === 403 ) return console.log("관리자가 아닌경우");
-        if ( status === 500 ) return console.log("서버 에러");
+            case 401:
+                console.log("토큰이 잘못되거나 없는경우");
+                break;
 
-    },[])
+            case 403:
+                console.log("관리자가 아닌경우");
+                break;
 
-    return alarm;
+            case 500:
+                console.log("서버 에러");
+                break;
+            default:
+                console.log("예상하지못한 경우")
+        }
+
+    },[ fetchData ])
+
+    return [ isNew ];
 };
