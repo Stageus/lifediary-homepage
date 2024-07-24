3c6842f360759012026223d69d430a75785c8969
junik <wkdwnsdlr37@gmail.com>
2024-07-24 13:56:45 +0900

    Refactor: usePutComplain 예외처리 변경

--- src/2_pages/complain/ui/complainItem/api/usePutComplain.js
+++ src/2_pages/complain/ui/complainItem/api/usePutComplain.js
@@ -1,35 +1,51 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useFetch, useCookie } from "@shared/hook";
 
-export const usePutComplain = ( isInvalid ) => {
+export const usePutComplain = () => {
 
+    const [ fetchData, baseFetch ] = useFetch();
     const { handleGetCookie } = useCookie();
-    const [ _, status, baseFetch ] = useFetch();
-    const [ itemState, setItemState ] = useState( isInvalid );
-    const [ saveState, setSaveState] = useState( null );
-
-    const changeState = () => setItemState( saveState );
+    const navigate = useNavigate();
 
     const putComplain = ( diaryIdx, isInvalid )=>{
-        // 200이라는 가정한 테스트 데이터
-        setSaveState(isInvalid);
-
-        // 임시주석
-        // baseFetch(`report/${diaryIdx}/status`, { isInvalid }, handleGetCookie());
-        changeState();
+        baseFetch(`report/${diaryIdx}/status`, {method:"PUT", data: isInvalid }, handleGetCookie());
     };
     
     useEffect(() => {
+        if ( !fetchData ) return;
 
-        if  ( status === 200 ) return changeState();
-        if  ( status === 400 ) return console.log("유효성 검사 실패일 경우");
-        if  ( status === 401 ) return console.log("토큰이 잘못된경우, 없는경우");
-        if  ( status === 403 ) return console.log("관리자가 아닐경우");
-        if  ( status === 404 ) return console.log("대상으로 하는 idx가 없는경우");
-        if  ( status === 500 ) return console.log("서버 에러");
+        switch ( fetchData.status ) {
+            case 200:
+                navigate(0);
+                break;
 
-    },[status]);
+            case 400:
+                console.log("유효성 검사 실패일 경우");
+                break;
 
-return [itemState, putComplain];
+            case 401:
+                console.log("토큰이 잘못된경우, 없는경우");
+                break;
+
+            case 403:
+                console.log("관리자가 아닐경우");
+                break;
+
+            case 404:
+                console.log("대상으로 하는 idx가 없는경우");
+                break;
+
+            case 500:
+                console.log("서버 에러");
+                break;
+
+            default:
+                console.log("예상하지못한 경우")
+        }
+
+    },[fetchData]);
+
+return [ putComplain ];
 
 }
