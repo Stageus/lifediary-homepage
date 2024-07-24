4ee60881e8d9b0ce15675294f173999c3822909b
mangkong1 <xogud5053@hanmail.net>
2024-07-24 18:21:29 +0900

    Docs: 로딩페이지 폴더 삭제

--- src/2_pages/login/ui/Login.jsx
+++ src/2_pages/login/ui/Login.jsx
@@ -4,21 +4,19 @@ import { S } from "./style";
 import { useGetRedirectUrl } from "../api/useGetRedirectUrl";
 import { useIsUser } from "../api/useIsUser";
 
-import { GoogleLoginBtn } from "@features/googleLogin/ui/GoogleLogin";
-
 export const Login = () => {
   const navigate = useNavigate();
 
-  const [ getRedirectUrl ] = useGetRedirectUrl();
-  const [ isUser ] = useIsUser();
+  const [getRedirectUrl] = useGetRedirectUrl();
+  const [isUser] = useIsUser();
 
   return (
     <S.PageContainer>
       <S.LoginContainer>
         <S.Logo onClick={() => navigate("/")} />
-        {/* 임시로 회원가입페이지로 즉시 이동하도록 경로설정, 추후 수정예정 */}
-        {/* <GoogleLoginBtn /> */}
-        <div onClick={getRedirectUrl}>google</div>
+        <S.GoogleLoginBtn onClick={getRedirectUrl}>
+          <S.GoogleLogo />
+        </S.GoogleLoginBtn>
       </S.LoginContainer>
     </S.PageContainer>
   );
