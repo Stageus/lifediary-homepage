512e14323571497812f2111802df53171bf83f3f
mangkong1 <xogud5053@hanmail.net>
2024-07-26 16:06:21 +0900

    Refactor: ProfileInfo api 코드 작성

--- src/2_pages/myProfile/ui/MyProfile.jsx
+++ src/2_pages/myProfile/ui/MyProfile.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { S } from "./style.js";
 import { MyProfileInfo } from "@features/myProfileInfo";
 import { MyDiaryList } from "@features/myDiaryList";
+import { LikeDiaryList } from "@features/likeDiaryList";
 
 export const MyProfile = () => {
   const [selectedTab, setSelectedTab] = useState("myDiaryTab");
@@ -19,7 +20,7 @@ export const MyProfile = () => {
           <S.TabBtnContainer onClick={() => handleTabBtnClick("myDiaryTab")}>{selectedTab === "myDiaryTab" ? <S.ActiveTabBtn>내 일기</S.ActiveTabBtn> : <S.DefaultTabBtn>내 일기</S.DefaultTabBtn>}</S.TabBtnContainer>
           <S.TabBtnContainer onClick={() => handleTabBtnClick("likeDiaryTab")}>{selectedTab === "likeDiaryTab" ? <S.ActiveTabBtn>좋아요 표시한 일기</S.ActiveTabBtn> : <S.DefaultTabBtn>좋아요 표시한 일기</S.DefaultTabBtn>}</S.TabBtnContainer>
         </S.ProfileTabContainer>
-        {selectedTab === "myDiaryTab" ? <MyDiaryList /> : null}
+        {selectedTab === "myDiaryTab" ? <MyDiaryList /> : <LikeDiaryList />}
       </S.MyProfileContentContainer>
     </>
   );
