d38342b6cfd5de588add47f0ddaccfe1f33ee3ff
mangkong1 <xogud5053@hanmail.net>
2024-07-29 14:06:40 +0900

    Refactor: userprofile pages레이어로 통합

---
+++ src/2_pages/diaryCreate/lib/createGrass.js
@@ -0,0 +1,38 @@
+import { useState, useEffect, useRef } from "react";
+
+export const createGrass = () => {
+  const [color, setColor] = useState("");
+  const [showPicker, setShowPicker] = useState(false);
+  const pickerRef = useRef();
+
+  const handleShowPicker = () => {
+    setShowPicker(true);
+  };
+
+  const handleColorChange = (color) => {
+    setColor(color.hex);
+  };
+
+  const handleRandomColor = () => {
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    setColor(randomColor);
+  };
+
+  useEffect(() => {
+    // 외부 클릭 감지 함수
+    const handleClickOutside = (event) => {
+      if (pickerRef.current && !pickerRef.current.contains(event.target)) {
+        setShowPicker(false);
+      }
+    };
+
+    // 문서에 클릭 이벤트 리스너 추가
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // 컴포넌트 언마운트 시 이벤트 리스너 제거
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return [pickerRef, color, showPicker, handleShowPicker, handleColorChange, handleRandomColor];
+};
