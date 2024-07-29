e284c022bbfaf8f4dbbf77905c279ce7fe4f0ccf
mangkong1 <xogud5053@hanmail.net>
2024-07-29 10:43:04 +0900

    Refacotr: MyProfile pages레이어로 통합

--- src/2_pages/userProfile/ui/UserProfile.jsx
+++ src/2_pages/userProfile/ui/UserProfile.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import { S } from "./style.js";
 import { UserProfileInfo } from "@features/userProfileInfo";
-import { UserDiaryList } from "@features/userDiaryList";
+import { MyDiaryList } from "@features/myDiaryList";
 
 export const UserProfile = () => {
   const [selectedTab, setSelectedTab] = useState("myDiaryTab");
@@ -18,7 +18,7 @@ export const UserProfile = () => {
         <S.ProfileTabContainer>
           <S.TabBtnContainer onClick={() => handleTabBtnClick("myDiaryTab")}>{selectedTab === "myDiaryTab" ? <S.ActiveTabBtn>유저 일기</S.ActiveTabBtn> : <S.DefaultTabBtn>유저 일기</S.DefaultTabBtn>}</S.TabBtnContainer>
         </S.ProfileTabContainer>
-        {selectedTab === "myDiaryTab" ? <UserDiaryList /> : null}
+        {selectedTab === "myDiaryTab" ? <MyDiaryList /> : null}
       </S.MyProfileContentContainer>
     </>
   );
