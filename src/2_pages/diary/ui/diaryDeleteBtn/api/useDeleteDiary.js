fa26c609a593c359566b30618f437e68a305a92e
junik <wkdwnsdlr37@gmail.com>
2024-07-30 16:14:27 +0900

    Refactor: diaryDeleteBtn 폴더 수정

--- src/2_pages/diary/ui/diaryDeleteBtn/api/useDeleteDiary.js
+++ src/2_pages/diary/ui/diaryDeleteBtn/api/useDeleteDiary.js
@@ -2,23 +2,49 @@ import { useEffect } from "react";
 import { useFetch, useCookie } from "@shared/hook";
 
 export const useDeleteDiary = ()=>{
-    const [ _, status, baseFetch ] = useFetch();
+    const [ fetchData, baseFetch ] = useFetch();
     const { handleGetCookie } = useCookie();
+
+    const onClickConfirm = () => {
+        const ConFirm = confirm("해당 일기를 삭제하시 겠습니까?");
+        if ( !ConFirm ) return;
+        deleteDiary(diaryIdx);
+    };
 
     const deleteDiary = ( diaryIdx ) => {
         baseFetch(`diary/${diaryIdx}`, {method: "DELETE"}, handleGetCookie());
     };
 
     useEffect(()=>{
+        if ( !fetchData ) return;
 
-        if  ( status === 200 ) return console.log("다른곳으로 리다이렉트");
-        if  ( status === 400 ) return console.log("유효성 감시 실패일 경우");
-        if  ( status === 401 ) return console.log("토큰이 잘못된 경우(없는경우)");
-        if  ( status === 403 ) return console.log("해당 일기의 주인이 아닌경우");
-        if  ( status === 404 ) return console.log("대상으로 하는 diaryIdx가 없는경우");
-        if  ( status === 500 ) return console.log("서버에러");
+        switch ( fetchData.status ) {
+            case 200:
+                console.log("다른곳으로 리다이렉트");
+                break;
 
-    },[status])
+            case 400:
+                console.log("유효성 감시 실패일 경우");
+                break;
 
-    return [ deleteDiary ];
+            case 401:
+                console.log("토큰이 잘못된 경우(없는경우)");
+                break;
+
+            case 403:
+                console.log("해당 일기의 주인이 아닌경우");
+                break;
+
+            case 404:
+                console.log("대상으로 하는 diaryIdx가 없는경우");
+                break;
+
+            case 500:
+                console.log("서버에러");
+                break;
+        }
+
+    },[ fetchData ])
+
+    return [ onClickConfirm ];
 }
--- src/2_pages/diary/ui/diaryDeleteBtn/ui/DiaryDeleteBtn.jsx
+++ src/2_pages/diary/ui/diaryDeleteBtn/ui/DiaryDeleteBtn.jsx
@@ -2,18 +2,13 @@ import { DefaultBtn } from "@shared/ui";
 import { useDeleteDiary } from "../api/useDeleteDiary";
 
 export const DiaryDeleteBtn = ( diaryIdx ) => {
-    const [ deleteDiary ] = useDeleteDiary();
 
-    const onClickConfirm = () => {
-        const ConFirm = confirm("해당 일기를 삭제하시 겠습니까?");
-        if ( !ConFirm ) return;
-        deleteDiary(diaryIdx);
-    };
+    const [ onClickConfirm ] = useDeleteDiary();
     
     return(
         <DefaultBtn
         text="일기삭제"
-        onClick={onClickConfirm}
+        onClick={()=> onClickConfirm( diaryIdx )}
         />
     );
 }
