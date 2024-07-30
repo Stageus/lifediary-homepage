e41d0aa1018f8f2a3e8315a32b9f91bd57ca8dfd
junik <wkdwnsdlr37@gmail.com>
2024-07-30 19:18:49 +0900

    Refactor: useSlider 명칭변경

---
+++ src/2_pages/diary/ui/diaryInfo/ui/slider/model/useSlider.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export const useSlider = ( listLength )=>{
+    const [ positionUnit, setPositionUnit ] = useState( 0 );
+
+    const onClickLeft = () => {
+        if ( positionUnit === 0 ) return;
+        setPositionUnit( positionUnit + 1 );
+    };
+
+    const onClickRight = () => {
+        if ( -positionUnit === listLength - 1 ) return;
+        setPositionUnit( positionUnit - 1 );
+    };
+
+    return { positionUnit, onClickLeft, onClickRight };
+}
--- src/2_pages/diary/ui/diaryInfo/ui/slider/ui/index.jsx
+++ src/2_pages/diary/ui/diaryInfo/ui/slider/ui/index.jsx
@@ -1,32 +1,32 @@
 import { S } from "./style";
-import { useModel } from "../model/useModel";
+import { useModel } from "../model/useSlider";
 import { Icon, DynamicImage } from "@shared/ui";
 
 export const Slider = (props) => {
-    const {sliderList} = props;
-    const {positionUnit, onClickLeft, onClickRight} = useModel(sliderList.length);
+    const { sliderList } = props;
+    const { positionUnit, onClickLeft, onClickRight } = useModel(sliderList.length);
 
   return (
     <>
       <S.Slider>
-        <S.LeftBtn onClick={onClickLeft}>
+        <S.LeftBtn onClick={ onClickLeft }>
           <Icon color="white" size={"40px"} type={"leftArrow"} />
         </S.LeftBtn>
-        <S.ItemContainer $positionUnit={positionUnit}>
+        <S.ItemContainer $positionUnit={ positionUnit }>
           {sliderList?.map((img, idx) => {
             return (
-                <div key={idx}>
-                    <DynamicImage src={img}/>
+                <div key={ idx }>
+                    <DynamicImage src={ img }/>
                 </div>
             );
           })}
         </S.ItemContainer>
-        <S.RightBtn onClick={onClickRight}>
+        <S.RightBtn onClick={ onClickRight }>
           <Icon color="white" size={"40px"} type={"rightArrow"} />
         </S.RightBtn>
         <S.ItemPosition>
           {sliderList?.map((_, idx) => {
-            return <S.Item key={idx} $isSame={-positionUnit === idx}/>;
+            return <S.Item key={ idx } $isSame={ -positionUnit === idx }/>;
           })}
         </S.ItemPosition>
       </S.Slider>
