dc309687926de67321828d5ae5bb753340ea4247
mangkong1 <xogud5053@hanmail.net>
2024-07-31 11:46:56 +0900

    Refactor: userProfile profileInfo수정

--- src/2_pages/userProfile/ui/UserProfile.jsx
+++ src/2_pages/userProfile/ui/UserProfile.jsx
@@ -3,10 +3,14 @@ import { useState } from "react";
 import { S } from "./style.js";
 import { UserDiaryList } from "./userDiaryList";
 import { DatePeriodPicker } from "./datePeriodPicker";
-import { ProfileInfo } from "./profileInfo";
+
+import { SubscribeBtn } from "@features/subscribeBtn";
+import DefaultProfile from "@shared/assets/imges/profile.png";
 
 export const UserProfile = () => {
   const [selectedTab, setSelectedTab] = useState("userDiaryTab");
+  const [profileImg, setProfileImg] = useState(DefaultProfile);
+  const [nickname, setNickname] = useState("고양이");
 
   const handleTabBtnClick = (e) => {
     setSelectedTab(e);
@@ -15,7 +19,16 @@ export const UserProfile = () => {
   return (
     <>
       <S.MyProfileContentContainer>
-        <ProfileInfo />
+        <S.ProfileInfoContainer>
+          {profileImg && <S.ProfileImg src={profileImg} />}
+          <S.NicknameAndSubscribeContainer>
+            <S.NicknameContainer>
+              <S.Nickname>{nickname}</S.Nickname>
+              <SubscribeBtn />
+            </S.NicknameContainer>
+            <S.SubscribeInfo>구독자 100명, 작성 일기 100개</S.SubscribeInfo>
+          </S.NicknameAndSubscribeContainer>
+        </S.ProfileInfoContainer>
         <S.ProfileTabContainer>
           <S.TabBtnContainer onClick={() => handleTabBtnClick("userDiaryTab")}>{selectedTab === "userDiaryTab" ? <S.ActiveTabBtn>유저 일기</S.ActiveTabBtn> : <S.DefaultTabBtn>유저 일기</S.DefaultTabBtn>}</S.TabBtnContainer>
         </S.ProfileTabContainer>
