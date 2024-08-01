f0c9de5494cebcc86baa19ca50d0930a27d0a14b
junik <wkdwnsdlr37@gmail.com>
2024-08-01 20:10:13 +0900

    Refactor: useCookie 수정

--- src/5_shared/hook/useCookie.js
+++ src/5_shared/hook/useCookie.js
@@ -1,21 +1,34 @@
 import { useCookies } from "react-cookie";
 
+
+/*
+  cookie의 사용처는 다양해야함
+  로그인했을시에 권한을 저장할쿠키
+  로그인했을시에 토큰을 저장할쿠키
+  로그아웃 했을시에 권한 & 토큰을 삭제할쿠키
+  로그인했을시에 사용자정보를 저장할 쿠키
+*/
 export const useCookie = () => {
-  const [cookies, setCookie, removeCookie] = useCookies(["myCookie"]);
 
-  const handleSetCookie = (name, value) => {
-    const expires = new Date();
-    expires.setDate(expires.getDate() + 1);
-    setCookie(name, value, { expires });
+  const [ cookies, setCookie, removeCookie ] = useCookies(["myCookie"]);
+  
+  const cookieSet = ( name, value, period ) => {
+
+    const newPeriod = new Date();
+    newPeriod.setDate(newPeriod.getDate() + 12 * 60 * 60 * 1000);
+
+    setCookie(name, value, { expires: period ?? newPeriod, path: "/" });
   };
 
-  const handleGetCookie = () => {
-    return cookies.myCookie;
+  const cookieGet = ( cookieName ) => {
+    return cookies[cookieName];
+  }
+
+  const cookieRemove = () => {
+    Object.keys(cookies).forEach( cookie => {
+      removeCookie(cookie);
+    })
   };
 
-  const handleRemoveCookie = (name) => {
-    removeCookie(name);
-  };
-
-  return { handleSetCookie, handleGetCookie, handleRemoveCookie };
+  return { cookieSet, cookieGet, cookieRemove };
 };
