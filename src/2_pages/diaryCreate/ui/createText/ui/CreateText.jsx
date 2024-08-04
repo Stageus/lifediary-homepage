2b87dd64ec297c37e119a551dfe891fff43fd24c
mangkong1 <xogud5053@hanmail.net>
2024-08-05 06:00:15 +0900

    Refactor: onChange대신 onBlur사용

--- src/2_pages/diaryCreate/ui/createText/ui/CreateText.jsx
+++ src/2_pages/diaryCreate/ui/createText/ui/CreateText.jsx
@@ -1,14 +1,22 @@
 import { S } from "./style";
 import { useCheckTextLength } from "../lib/useCheckTextLength";
+import { useEffect, useRef } from "react";
 
 export const CreateText = () => {
   const [checkTextLength, textRef] = useCheckTextLength();
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.value = textRef.current;
+    }
+  }, [textRef.current]);
 
   return (
     <>
       <S.ContentContainer>
         <S.ContentNameContainer>내용</S.ContentNameContainer>
-        <S.TextContent onChange={checkTextLength} value={textRef.current} placeholder="내용을 입력해주세요(최대 500자)" />
+        <S.TextContent onBlur={checkTextLength} ref={inputRef} placeholder="내용을 입력해주세요(최대 500자)" />
       </S.ContentContainer>
     </>
   );
