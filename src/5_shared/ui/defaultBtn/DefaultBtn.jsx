1037dedd8dd92ac353082b9bd58967e37bb99779
junik <wkdwnsdlr37@gmail.com>
2024-08-06 15:24:32 +0900

    Feat: ProfileTag추가

--- src/5_shared/ui/defaultBtn/DefaultBtn.jsx
+++ src/5_shared/ui/defaultBtn/DefaultBtn.jsx
@@ -1,12 +1,13 @@
 import { S } from "./style";
 
 export const DefaultBtn = ( props )=>{
-    const { text="base", size, type, onClick = null } = props;
+    const { text="base", size, type, onClick = null, shadow = true} = props;
     return(
         <>
             <S.DefaultBtn 
-            type={type}
-            size={size}
+            $type={type}
+            $size={size}
+            $shadow={shadow}
             onClick={ type !== 'disabled' ? ()=>onClick() : null}
             >{text}</S.DefaultBtn>
         </>
--- src/5_shared/ui/index.js
+++ src/5_shared/ui/index.js
@@ -2,3 +2,4 @@ export { Icon } from "./icon/Icon";
 export { TextInput } from "./textInput/TextInput";
 export { DefaultBtn } from "./defaultBtn/DefaultBtn";
 export { DynamicImage } from "./dynamicImage/DynamicImage";
+export { ProfileTag } from "./profileTag/ui/ProfileTag";
