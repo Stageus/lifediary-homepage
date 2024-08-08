f522448271050a00f31556640fe84b34318251b4
junik <wkdwnsdlr37@gmail.com>
2024-08-08 22:31:16 +0900

    Design: 정적 디자인 설정

--- src/2_pages/signUp/ui/SignUp.jsx
+++ src/2_pages/signUp/ui/SignUp.jsx
@@ -3,12 +3,48 @@
 import { S } from "./style";
 // Layer
 import logo from "@shared/assets/img/logo.png"
+import { Profile, DefaultBtn } from "@shared/ui";
 
 
 export const SignUp = () => {
     return(
-        <>
-        
-        </>
+        <S.signUp>
+            <S.innerBox>
+                {/* 로고영역 */}
+            <S.logoArea>
+                <img
+                src={logo}
+                />
+            </S.logoArea>
+            
+            {/* 프로필 영역 */}
+            <S.profileArea>
+                <S.imgWrap>
+                    <Profile/>
+                </S.imgWrap>
+                <S.profileGuide>확장자안내 및 필수문구?</S.profileGuide>
+            </S.profileArea>
+
+            {/* 이름 영역 */}
+            <S.nameArea>
+                <S.nameInout
+                placeholder="사용하실 이름을 입력해주세요"
+                />
+                <S.nameGuide>확인영역</S.nameGuide>
+            </S.nameArea>
+
+            {/* 버튼 영역 */}
+            <S.btnArea>
+                <DefaultBtn
+                    text="가입하기"
+                />
+                <DefaultBtn
+                    text="취소"
+                />
+            </S.btnArea>
+            </S.innerBox>
+
+            
+        </S.signUp>
     );
 }
--- src/2_pages/signUp/ui/style.js
+++ src/2_pages/signUp/ui/style.js
@@ -1,5 +1,82 @@
 import styled from "styled-components";
 
+const signUp = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+`;
+
+const innerBox = styled.div`
+    min-width: 525px;
+    height: 600px;
+    border: 4px solid ${({theme}) => theme.major};
+    border-radius: 12px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
+`;
+
+const logoArea = styled.div`
+    width: fit-content;
+
+    & > img {
+        border-radius: 8px;
+        width: 250px;
+    }
+`;
+
+const profileArea = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 5px;
+`;
+
+const imgWrap = styled.div`
+    width: 100px;
+`;
+
+const profileGuide = styled.span`
+    color: red;
+    font-size: 14px;
+`;
+
+const nameArea = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+`;
+
+const nameInout = styled.input`
+    width: 200px;
+    padding: 10px;
+    border-radius: 8px;
+    border: 2px solid ${({theme}) => theme.major};
+    font-size: 12px;
+`;
+
+const nameGuide = styled.span`
+    color: red;
+    font-size: 14px;
+`;
+
+const btnArea = styled.div`
+    display: flex;
+    gap: 10px;
+`;
+
 export const S = {
-    
+    signUp,
+    innerBox,
+    logoArea,
+    profileArea,
+    imgWrap,
+    profileGuide,
+    nameArea,
+    nameInout,
+    nameGuide,
+    btnArea,
 };
