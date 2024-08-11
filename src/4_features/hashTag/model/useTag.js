ac039e6beed7d161c371908de01f1bb2ac73f5c1
junik <wkdwnsdlr37@gmail.com>
2024-08-11 14:42:47 +0900

    Feat: hashTag 생성

---
+++ src/4_features/hashTag/model/useTag.js
@@ -0,0 +1,44 @@
+// Npm
+import { useRef } from "react";
+// Layer
+import { tagValidation } from "@shared/consts/validation";
+import { useMessage } from "@shared/store";
+
+export const useTag = ( props ) => {
+
+
+    const { tagList, setTagList } = props ?? [];
+    const inputRef = useRef(null);
+    const setMessage = useMessage( state => state.setMessage);
+
+    const onKeyUp = ( e ) => {
+        const target = e.target.value;
+        if (e.key === "Enter" && target.trim() !== "") {
+          if ( !tagValidation(target) ) {
+            setMessage("#은 사용할수 없으며,최대20자까지만 입력이 가능해요"); 
+            return;
+          }
+          setTagList([...tagList, target]);
+          e.target.value = "";
+        }
+      };
+    
+      const onBlur = ( e ) => {
+        const target = e.target.value;
+        if (target.trim() !== "") {
+          if ( !tagValidation(target) ) {
+            inputRef.current.focus();
+            return;
+          }
+          setTagList([...tagList, target]);
+          e.target.value = "";
+        }
+      };
+    
+      const deleteTag = ( text ) => {
+        const deleteTagList = tagList.filter((item) => item !== text);
+        setTagList(deleteTagList);
+      };
+
+    return { tagList, inputRef, onKeyUp, onBlur, deleteTag };
+}
