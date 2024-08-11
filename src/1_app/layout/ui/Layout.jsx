b2edb264633ae762c3a99818b898fa8b7e371a74
junik <wkdwnsdlr37@gmail.com>
2024-08-11 09:36:56 +0900

    Design: 페이지별 layout 재적용

--- src/1_app/layout/ui/Layout.jsx
+++ src/1_app/layout/ui/Layout.jsx
@@ -1,36 +1,42 @@
 // Npm
 import { Outlet, useLocation } from "react-router-dom";
 // Slice
-import { S } from "./style"; 
+import { S } from "./style";
 import { AlarmModal } from "../ui/alarmModal";
 import { MessageModal } from "./messageModal";
 // Layer
 import { Header } from "@app/header";
 import { Aside } from "@app/aside";
-import { paths } from "@shared/consts/paths"; 
+import { paths } from "@shared/consts/paths";
 
-export const Layout = ()=>{
+export const Layout = () => {
+  const loaction = useLocation();
+  const authPaths = [paths.LOGIN, paths.SIGNUP];
+  const isAuthPage = authPaths.some((path) => location.pathname.includes(path));
+  const isAside = Object.values(paths).some((item) =>
+    loaction.pathname.includes(item)
+  );
 
-    const loaction = useLocation();
-    const isLayout = Object.values(paths).some( item => loaction.pathname.includes(item));
-    
-    return(
-        <S.Layout>
-            <MessageModal/>
-            <S.header>
-                <Header/>
-            </S.header>
-            <S.divideArea>
-                { isLayout 
-                ? <S.aside>
-                    <Aside currentPage={isLayout}/>
-                </S.aside>
-                : null}
-                <S.main>
-                    <AlarmModal/>
-                    <Outlet/>
-                </S.main>
-            </S.divideArea>
-        </S.Layout>
-    );
-}
+  return (
+    <S.Layout>
+      <MessageModal />
+      {!isAuthPage && (
+        <S.header>
+          <Header />
+        </S.header>
+      )}
+
+      <S.divideArea>
+        {!isAuthPage && isAside ? (
+          <S.aside>
+            <Aside/>
+          </S.aside>
+        ) : null}
+        <S.main>
+          <AlarmModal />
+          <Outlet />
+        </S.main>
+      </S.divideArea>
+    </S.Layout>
+  );
+};
