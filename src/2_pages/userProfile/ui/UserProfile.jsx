6e8d1953ee40e786190c9020dd8b78242a03c55e
junik <wkdwnsdlr37@gmail.com>
2024-08-16 02:20:30 +0900

    Docs: useProfile Api 설정

--- src/2_pages/userProfile/ui/UserProfile.jsx
+++ src/2_pages/userProfile/ui/UserProfile.jsx
@@ -1,13 +1,16 @@
 // Slice
 import { S } from "./style";
+import { useGetAccountInfo } from "../api/useGetAccountInfo";
 // Layer
 import { AccountProfile } from "@widgets/accountProfile";
 
 export const UserProfile = () => {
 
+    const [ accountInfo ] = useGetAccountInfo();
+
     return (
         <S.userProfile>
-            <AccountProfile/>
+            <AccountProfile {...accountInfo}/>
         </S.userProfile>
     );
 }
