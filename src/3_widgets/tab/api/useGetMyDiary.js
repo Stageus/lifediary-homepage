1de6eeeac4d11e570102ef6faa7116aeb3174fce
junik <wkdwnsdlr37@gmail.com>
2024-08-16 13:02:31 +0900

    Docs: tap 파일 및 폴더구조 정의

---
+++ src/3_widgets/tab/api/useGetMyDiary.js
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { useFetch, useRoute, useCookie } from "@shared/hook";
+import { useMessage } from "@shared/store";
+
+export const useGetMyDiary = () => {
+
+    const [ fetchData, baseFetch ] = useFetch();
+    const { errorRoute } = useRoute();
+    const setMessage = useMessage( state => state.setMessage );
+    const { cookieGet } = useCookie();
+
+    const getMyDiary = () => {
+        baseFetch()
+    };
+
+    useEffect(()=>{
+        if ( !fetchData ) return;
+
+        switch ( fetchData.status ) {
+            case 200:
+                break;
+            case 400:
+                break;
+            case 401:
+                break;
+            case 404:
+                break;
+            case 500:
+                errorRoute(500, "서버에러");
+                break;
+        }
+
+    },[]);
+    
+
+    return [];
+}
+
---
+++ src/3_widgets/tab/ui/Tab.jsx
@@ -0,0 +1,10 @@
+import { S } from "./style"; 
+
+export const Tab = () => {
+    
+    return(
+        <div>
+            탭창
+        </div>
+    );
+}
