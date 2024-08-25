5bc68d167b39dd97365d802ecea499f8c9be0ad8
junik <wkdwnsdlr37@gmail.com>
2024-08-25 13:32:14 +0900

    Refactor: useGetAuth 주석추가

--- src/1_app/layout/api/useGetAuth.js
+++ src/1_app/layout/api/useGetAuth.js
@@ -28,6 +28,7 @@ export const useGetAuth = () => {
   };
 
   useEffect(() => {
+    // cookie 목록중 token이 있을경우에만 실행한다.
     if ( cookieGet("token") ) getAuth();
   }, [ cookieGet("token") ]);
 
@@ -37,6 +38,9 @@ export const useGetAuth = () => {
     switch ( fetchData.status ) {
       case 200:
         const mapperData = mapper(fetchData.data);
+        // cookie에 권한, 접속중인 유저의 이미지를 포함한 이유는?
+        // 권한은 신고페이지에 대한 접근권한때문에 저장
+        // 프로필이미지는 댓글에 답글달경우에, ux적으로 이용하기 위해
         cookieSet("profile",mapperData.profileImg);
         cookieSet("permission",mapperData.permission);
         setUserInfo( mapperData );
